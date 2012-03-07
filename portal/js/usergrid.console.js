572d34a5239d45b1ec09f3aff9c613f37df6699c
Rod Simpson <rod@apigee.com>
2012-03-07 11:46:16 -0800

    updated password regex and corresponding message

--- portal/js/usergrid.console.js
+++ portal/js/usergrid.console.js
@@ -9,7 +9,8 @@ function usergrid_console_app() {
     var self = this;
 
     var emailRegex = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i;
-    var passwordRegex = /^([0-9a-zA-Z@#$%^&])+$/;
+    var passwordRegex = /^([0-9a-zA-Z@#$%^&?!<>;:.|,'"~*-_=+\[\]\(\)\{\}\\/])+$/;
+    var passwordAllowedCharsMessage = 'Password field only allows: `0-9, a-z, A-Z, ~ @ # % ^ & * ( ) - _ = + [ ] { } \\ | ; : \' " , . < > / ? !';
     var usernameRegex = /^([0-9a-zA-Z\.\-])+$/;
     var nameRegex = /^([ 0-9a-zA-Z\.\-])+$/;
     var alphaNumRegex = /^([0-9a-zA-Z])+$/;
@@ -965,7 +966,7 @@ function usergrid_console_app() {
             && checkLength2(new_user_email, 6, 80)
             && checkRegexp2(new_user_email,emailRegex,"eg. ui@jquery.com")
             && checkLength2(new_user_password, 5, 16)
-            && checkRegexp2(new_user_password,passwordRegex,"Password field only allows : a-z, 0-9, @, #, $, %, ^, &");
+            && checkRegexp2(new_user_password,passwordRegex, passwordAllowedCharsMessage);
 
         if (bValid) {
             var data = form.serializeObject();
@@ -2771,7 +2772,7 @@ function usergrid_console_app() {
         }
         var password = $("#signup-password").val();
         if (!(passwordRegex.test(password))) {
-            displaySignupError("Invalid password, only a-z, 0-9, @, #, $, %, ^, &.");
+            displaySignupError(passwordAllowedCharsMessage);
             return;
         }
         if (password != $("#signup-password-confirm").val()) {
@@ -2859,7 +2860,7 @@ function usergrid_console_app() {
                 return;
             }
             if (!(passwordRegex.test(new_pass))) {
-                $.jAlert(client.getLastErrorMessage("Password field only allows : a-z, 0-9, @, #, $, %, ^, &"), 'error', function(result) {
+                $.jAlert(client.getLastErrorMessage(passwordAllowedCharsMessage), 'error', function(result) {
                      requestAccountSettings();
                  });
                 return;
