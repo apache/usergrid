87174919e92eaaef0c5ce49b65763cb6c34797e1
Fabian Jendle <fjendle@nearbpo.com>
2012-06-12 17:25:30 -0500

    deleted usergrid.data.js (empty) and cleanup of other *.js files

--- portal/js/usergrid.pages.js
+++ portal/js/usergrid.pages.js
@@ -1,117 +1,117 @@
 function UsergridPages(){
-	var self = {
-		pages: {},
-		panels: {},
+  var self = {
+    pages: {},
+    panels: {},
     resetPasswordUrl: ''
-	}
+  }
 
   self.clearPage = function(){
     $("#pages > div").hide();
   }
 
-	self.ShowPage = function(pageName){
+  self.ShowPage = function(pageName){
     console.log('showing ' +pageName);
-		$("#pages > div").hide();
-		var page = self.pages[pageName];
-		page.box.show();
-		$(".navbar li.active").removeClass('active');
-		$(".navbar .navbar-inner").hide();
+    $("#pages > div").hide();
+    var page = self.pages[pageName];
+    page.box.show();
+    $(".navbar li.active").removeClass('active');
+    $(".navbar .navbar-inner").hide();
 
     if(page.link.parent().parent().hasClass("dropdown-menu")) {
-        page.link.parent().parent().parent().addClass('active');
+      page.link.parent().parent().parent().addClass('active');
     } else {
       page.link.parent().addClass('active');
       page.menu.show();
     }
 
     if(page.showFunction) {
-			page.showFunction();
+      page.showFunction();
     }
 
-		return;
-	}
+    return;
+  }
 
-	self.AddPage = function(page)	{
-		if(!page.link)
-            page.link = $("#" + page.name + '-link');
+  self.AddPage = function(page) {
+    if(!page.link)
+      page.link = $("#" + page.name + '-link');
 
-		if(!page.box)
-			page.box = $("#" + page.name + '-page');
+    if(!page.box)
+      page.box = $("#" + page.name + '-page');
 
-		page.link.click(function(e) {
-			e.preventDefault();
-            if(!page.link.hasClass("dropdown-toggle"))
-    			self.ShowPage(page.name);
-		});
+    page.link.click(function(e) {
+      e.preventDefault();
+      if(!page.link.hasClass("dropdown-toggle"))
+        self.ShowPage(page.name);
+    });
 
-		LoadPage(page);
-		self.pages[page.name] = page;
-	}
+    LoadPage(page);
+    self.pages[page.name] = page;
+  }
 
-	self.AddPanel = function(panelName, linkSelector,boxSelector,initFunction,showFunction)	{
-		if(!linkSelector)
-			linkSelector = "#sidebar-menu a[href='#" + panelName + "']";
+  self.AddPanel = function(panelName, linkSelector,boxSelector,initFunction,showFunction)       {
+    if(!linkSelector)
+      linkSelector = "#sidebar-menu a[href='#" + panelName + "']";
 
-		if(!boxSelector)
-			boxSelector = "#" + panelName + '-panel';
+    if(!boxSelector)
+      boxSelector = "#" + panelName + '-panel';
 
-		var panel = {
-			name: panelName,
-			link: $(linkSelector),
-			box:$(boxSelector),
-			initFunction:initFunction,
-			showFunction:showFunction
-		}
+    var panel = {
+      name: panelName,
+      link: $(linkSelector),
+      box:$(boxSelector),
+      initFunction:initFunction,
+      showFunction:showFunction
+    }
 
-        if(panel.initFunction)
-            panel.initFunction();
+    if(panel.initFunction)
+      panel.initFunction();
 
-		panel.link.click(function(e) {
-			e.preventDefault();
-			self.SelectPanel(panel.name);
-		});
+    panel.link.click(function(e) {
+      e.preventDefault();
+      self.SelectPanel(panel.name);
+    });
 
-		self.panels[panel.name] = panel;
-	}
+    self.panels[panel.name] = panel;
+  }
 
-	self.SelectPanel = function (panelName){
-		var panel = self.panels[panelName];
+  self.SelectPanel = function (panelName){
+    var panel = self.panels[panelName];
 
-		$("#sidebar-menu li.active").removeClass('active');
-		panel.link.parent().addClass('active');
+    $("#sidebar-menu li.active").removeClass('active');
+    panel.link.parent().addClass('active');
 
-		if(panel.showFunction){
-			panel.showFunction();
-		}
-		
-        if (panelName == 'console') {
-            url = usergrid.console.getAccessTokenURL();
-            $("#console-panel iframe").attr("src", url);
-        }
+    if(panel.showFunction){
+      panel.showFunction();
+    }
 
-		$("#console-panels > div").hide();
-		panel.box.show();
+    if (panelName == 'console') {
+      url = usergrid.console.getAccessTokenURL();
+      $("#console-panel iframe").attr("src", url);
+    }
 
-	}
+    $("#console-panels > div").hide();
+    panel.box.show();
 
-	function LoadPage(page){
+  }
 
-		if(page.name=='forgot-password')
-			$("#forgot-password-page iframe").attr("src", self.resetPasswordUrl);
-        else if(page.name=='console-frame')
-      		$("#console-frame-page iframe").attr("src", "consoleFrame.html");
-		else{
-			if(window.location.pathname.indexOf('app') > 0){
-				$.ajaxSetup ({cache: false});
-				page.box.load(page.name + '.html',page.initFunction);
-				$.ajaxSetup ({cache: true});
-			}
-			else if(page.initFunction)
-				page.initFunction();
-		}
+  function LoadPage(page){
 
-		return;
-	}
+    if(page.name=='forgot-password')
+      $("#forgot-password-page iframe").attr("src", self.resetPasswordUrl);
+    else if(page.name=='console-frame')
+      $("#console-frame-page iframe").attr("src", "consoleFrame.html");
+    else{
+      if(window.location.pathname.indexOf('app') > 0){
+        $.ajaxSetup ({cache: false});
+        page.box.load(page.name + '.html',page.initFunction);
+        $.ajaxSetup ({cache: true});
+      }
+      else if(page.initFunction)
+        page.initFunction();
+    }
 
-	return self;
+    return;
+  }
+
+  return self;
 }
