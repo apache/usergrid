9fc0922c6fbc91ca18eae5a9f2b0acfb70352a3f
Ed Anuff <ed@anuff.com>
2012-06-25 11:26:53 -0700

    stub out copying of activities

--- stack/services/src/main/java/org/usergrid/services/users/following/FollowingService.java
+++ stack/services/src/main/java/org/usergrid/services/users/following/FollowingService.java
@@ -1,15 +1,21 @@
 package org.usergrid.services.users.following;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.usergrid.persistence.ConnectionRef;
 import org.usergrid.persistence.EntityRef;
 import org.usergrid.services.AbstractConnectionsService;
 
 public class FollowingService extends AbstractConnectionsService {
 
+	private static final Logger logger = LoggerFactory
+			.getLogger(FollowingService.class);
+
 	@Override
 	public ConnectionRef createConnection(EntityRef connectingEntity,
 			String connectionType, EntityRef connectedEntityRef)
 			throws Exception {
+		copyActivityFeed(connectingEntity, connectedEntityRef);
 		return em.createConnection(connectingEntity, connectionType,
 				connectedEntityRef);
 	}
@@ -17,6 +23,11 @@ public class FollowingService extends AbstractConnectionsService {
 	@Override
 	public void deleteConnection(ConnectionRef connectionRef) throws Exception {
 		em.deleteConnection(connectionRef);
+	}
+
+	public void copyActivityFeed(EntityRef connectingEntity,
+			EntityRef connectedEntityRef) {
+		logger.info("Copying activities...");
 	}
 
 }
