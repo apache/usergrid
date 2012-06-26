782c8e3214bf554e3962a0e07a2467b7f46f1769
Fabian Jendle <fjendle@nearbpo.com>
2012-06-26 16:08:58 -0500

    regexp's tweaks

--- portal/js/app/console.js
+++ portal/js/app/console.js
@@ -10,21 +10,21 @@ function usergrid_console_app(Pages) {
 
   var self = this;
 
-  var emailRegex = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i;
+  var emailRegex = new RegExp("^([ 0-9 a-z A-Z ]+([ \_ \. \- \+ ]?[ 0-9 a-z A-Z ]+)*@[ 0-9 a-z A-Z ] + [ 0-9, a-z, A-Z, \., \- ]*(.){1}[ a-z A-Z ]{2,4})+$");
   var emailAllowedCharsMessage = 'eg. example@apigee.com';
-  var passwordRegex = /^([0-9a-zA-Z@#$%^&?!<>;:.|,'"~*-_=+\[\]\(\)\{\}\\/])+$/;
+  var passwordRegex = new RegExp("^([ 0-9 a-z A-Z @ # $ % ^ & ? ! < > ; : . | , ' \" ~ * - _ = + \[ \] \( \) \{ \} \\ / ])+$");
   var passwordAllowedCharsMessage = 'Password field only allows: A-Z, a-z, 0-9, ~ @ # % ^ & * ( ) - _ = + [ ] { } \\ | ; : \' " , . < > / ? !';
-  var usernameRegex = /^([0-9a-zA-Z\.\-])+$/;
+  var usernameRegex = new RegExp("^([ 0-9 a-z A-Z \. \- ])+$");
   var usernameAllowedCharsMessage = 'Username field only allows : A-Z, a-z, 0-9, dot, and dash';
-  var organizatioNameRegex = /^([0-9a-zA-Z\.\-])+$/;
+  var organizatioNameRegex = new RegExp ("^([ 0-9 a-z A-Z \. \- ])+$");
   var organizationNameAllowedCharsMessage = 'Organization name field only allows : A-Z, a-z, 0-9, dot, and dash';
-  var nameRegex = /^([ 0-9a-zA-Z@#$%^&?!;:.|,'"~*-_=+\[\]\(\)\{\}\\/])+$/;
+  var nameRegex = new RegExp("^([ 0-9 a-z A-Z \@ \# \$ \% \^ \& \? \! \; \: \. \| \, \' \" \~ \* \- \_ \= \+ \[ \] \( \) \{ \} \\ \/ ])+$");
   var nameAllowedCharsMessage = 'Name field only allows: A-Z, a-z, 0-9, ~ @ # % ^ & * ( ) - _ = + [ ] { } \\ | ; : \' " , . / ? !';
-  var titleRegex = /^([ 0-9a-zA-Z\.\-!?/])+$/;
+  var titleRegex = new RegExp("^([ 0-9 a-z A-Z \. \- \! \? \/ ])+$");
   var titleAllowedCharsMessage = 'Title field only allows : space, A-Z, a-z, 0-9, dot, dash, /, !, and ?';
-  var alphaNumRegex = /^([0-9a-zA-Z])+$/;
+  var alphaNumRegex = new RegExp("^([ 0-9 a-z A-Z ])+$");
   var alphaNumAllowedCharsMessage = 'Collection name only allows : a-z 0-9';
-  var pathRegex = /^([0-9a-z\.\-\/])+$/;
+  var pathRegex = new RegExp("^([ 0-9 a-z \. \- \/ ])+$");
   var pathAllowedCharsMessage = 'Path only allows : /, a-z, 0-9, dot, and dash';
   var roleAllowedCharsMessage = 'Path only allows : /, a-z, 0-9, dot, and dash';
 
