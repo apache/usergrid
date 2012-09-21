4909650297b56ecbbc4908880438d76bf0af317a
Scott Ganyo <sganyo@apigee.com>
2012-09-21 15:55:00 -0700

    fix rspec deprecation

--- sdks/ruby/spec/usergrid/extensions/hash_spec.rb
+++ sdks/ruby/spec/usergrid/extensions/hash_spec.rb
@@ -4,7 +4,7 @@ describe Hash do
 
   it "should add dot notation recursively" do
     h = { test1: 'test1', test2: { test2a: 'test2a' }, test3: [ { test3a: 'test3a' }] }
-    expect { h.test1 }.should raise_error
+    expect { h.test1 }.to raise_error
     h.add_dot_notation!
     h.test1.should eq 'test1'
     h.test2.test2a.should eq 'test2a'
