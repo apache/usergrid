8ef30b440d99eb181ed0f283c6f7bf33b838fffa
Todd Nine <tnine@apigee.com>
2012-10-04 15:33:59 -0700

    Fixes broken mongo test

--- stack/mongo-emulator/src/test/java/org/usergrid/mongo/MongoQueryTest.java
+++ stack/mongo-emulator/src/test/java/org/usergrid/mongo/MongoQueryTest.java
@@ -461,19 +461,19 @@ public class MongoQueryTest extends AbstractMongoTest {
     properties.put("name", "Kings of Leon");
     properties.put("genre", "Southern Rock");
     properties.put("founded", 2000);
-    em.create("testand", properties);
+    em.create("withfieldselector", properties);
 
     properties = new LinkedHashMap<String, Object>();
     properties.put("name", "Stone Temple Pilots");
     properties.put("genre", "Rock");
     properties.put("founded", 1986);
-    em.create("testand", properties);
+    em.create("withfieldselector", properties);
 
     properties = new LinkedHashMap<String, Object>();
     properties.put("name", "Journey");
     properties.put("genre", "Classic Rock");
     properties.put("founded", 1973);
-    em.create("testand", properties);
+    em.create("withfieldselector", properties);
 
     Mongo m = new Mongo("localhost", 27017);
 
@@ -488,7 +488,7 @@ public class MongoQueryTest extends AbstractMongoTest {
 
     //query.put();
 
-    DBCollection coll = db.getCollection("testands");
+    DBCollection coll = db.getCollection("withfieldselectors");
     DBCursor cur = coll.find(queryName, limitName);
 
     assertTrue(cur.hasNext());
