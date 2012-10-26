bd15bfb3a5b564777c2e8d3ddbf9883d6cab648f
ubermondo <dieter@ubermondo.com>
2012-10-26 12:20:05 -0400

    removing server address from invocation

--- sdks/nodejs/test/server.js
+++ sdks/nodejs/test/server.js
@@ -70,9 +70,10 @@ function start(route, handle) {
     sdk.Usergrid.session.save_session();
   }
 
-  http.createServer(onRequest).listen(8888, '127.0.0.1');
+  //http.createServer(onRequest).listen(8888, '127.0.0.1');
+  http.createServer(onRequest).listen(8888);
   console.log("Server has started.");
-  console.log('Server running at http://127.0.0.1:8888/');
+  console.log('Server running at port 8888');
 }
 
 function serveAsset(response, filePath, contentType) {
