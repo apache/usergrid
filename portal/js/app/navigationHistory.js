927f705cc3345e8eeeb6ef071a4138091b051328
Tarek Peregrina <tperegrina@nearbpo.com>
2012-11-22 18:02:51 -0600

    Started Usergrid.Navigation, will probably go back on this changes.
    [Issue: 315-Usergrid]

---
+++ portal/js/app/navigationHistory.js
@@ -0,0 +1,23 @@
+/*
+  *Usergrid.Navigation
+  *
+  * Functions to control the navigation of the Console client
+  *
+  * Uses: jQuery.Address
+  *
+  * Requires:
+  *
+  */
+
+(function () {
+
+  Usergrid.Navigation = function() {};
+
+  Usergrid.Navigation.prototype = {
+    default : {
+
+    },
+
+
+  }
+})(Usergrid);
