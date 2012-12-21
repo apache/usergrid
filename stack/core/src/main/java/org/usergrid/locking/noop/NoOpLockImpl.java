a6d193166b9a5d3e15c3e2a71808548ce25cd0ff
Todd Nine <tnine@apigee.com>
2012-12-21 17:27:52 -0500

    Changed no-op lock package.
    Made hector locks the new default
    
    USERGRID-1120

--- stack/core/src/main/java/org/usergrid/locking/noop/NoOpLockImpl.java
+++ stack/core/src/main/java/org/usergrid/locking/noop/NoOpLockImpl.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
-package org.usergrid.locking.zookeeper;
+package org.usergrid.locking.noop;
 
 import java.util.concurrent.TimeUnit;
 
