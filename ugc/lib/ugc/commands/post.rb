21c1e8270e13643548d7c092f936b5f4ecd0c52d
Scott Ganyo <scott@ganyo.com>
2013-01-03 17:30:40 -0800

    added create,update aliases for post,put. updated docs.

--- ugc/lib/ugc/commands/post.rb
+++ ugc/lib/ugc/commands/post.rb
@@ -1,13 +1,13 @@
-desc 'http post'
+desc 'non-idempotent create or update (usually create)'
 arg_name 'url [data]'
 
-command :post do |c|
+command :post,:create do |c|
   c.flag [:d,:data]
 
   c.action do |global_options,options,args|
     help_now! unless args[0]
 
-    format_result resource(args[0]).post (options[:data] || args[1])
+    format_result resource(args[0]).post parse_data(options[:data] || args[1])
   end
 
 end
--- ugc/lib/ugc/commands/put.rb
+++ ugc/lib/ugc/commands/put.rb
@@ -1,13 +1,13 @@
-desc 'http put'
+desc 'idempotent create or update (usually an update)'
 arg_name 'url [data]'
 
-command :put do |c|
+command :put,:update do |c|
   c.flag [:d,:data]
 
   c.action do |global_options,options,args|
     help_now! unless args[0]
 
-    format_result resource(args[0]).put (options[:data] || args[1])
+    format_result resource(args[0]).put parse_data(options[:data] || args[1])
   end
 
 end
