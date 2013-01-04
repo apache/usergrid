3a7020129aeb08e575e5d6640d03338759abd70c
Scott Ganyo <scott@ganyo.com>
2013-01-03 17:29:16 -0800

    merged list.rb into get.rb

--- ugc/lib/ugc/commands/get.rb
+++ ugc/lib/ugc/commands/get.rb
@@ -1,12 +1,50 @@
-desc 'http get'
-arg_name 'url'
+desc 'retrieve a collection or entity'
 
-command :get do |c|
+command :get,:show,:ls,:list do |c|
 
-  c.action do |global_options,options,args|
-    help_now! unless args[0]
+  c.desc 'url'
+  c.command [:url] do |c2|
 
-    format_result resource(args[0]).get
+    c2.action do |global_options,options,args|
+      help_now! unless args[0]
+
+      format_result $application[args[0]].get
+    end
   end
+
+  c.desc 'collections'
+  c.command [:collections] do |c2|
+
+    c2.action do |global_options,options,args|
+      app = $application.entity
+      collections = app['metadata']['collections']
+      table border: true do
+        row header: true do
+          collections.first[1].each_key do |k|
+            column k
+          end
+        end
+        collections.each_value do |coll|
+          row do
+            coll.each_value do |v|
+              column v
+            end
+          end
+        end
+      end
+    end
+  end
+
+  default_names = %w(assets users events roles folders activities devices groups)
+  default_names.each do |e|
+    c.desc e
+    c.command [e.to_sym] do |c2|
+      c2.action do |global_options,options,args|
+        format_collection($application[e].collection)
+      end
+    end
+  end
+
+  c.default_command :url
 
 end
