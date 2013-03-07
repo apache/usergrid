48ffd5da57ebb3d3e41634bdd86ae3db1dd61da0
Rod Simpson <rod@apigee.com>
2013-03-07 16:38:35 -0700

    updated SSO callback

--- portal/js/app/sso.js
+++ portal/js/app/sso.js
@@ -16,7 +16,7 @@
     use_sso: true, // flag to override use SSO if needed set to ?use_sso=no
     login_url: "https://accounts.apigee.com/accounts/sign_in",
     profile_url: "https://accounts.apigee.com/accounts/my_account",
-    logout_url: "https://accounts.apigee.com/accounts/sign_out",
+    logout_url: "https://accounts.apigee.com/accounts/sign_in",
     api_url: "https://api.usergrid.com/"
     },
 
