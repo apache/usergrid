be12f6f06a1f646f14a7a7c56a977452262c4d2f
Todd Nine <tnine@apigee.com>
2013-03-20 19:01:41 -0700

    Fixed dynamic put and load to return correct runtime types
    USERGRID-1185

--- stack/core/src/main/java/org/usergrid/persistence/AbstractEntity.java
+++ stack/core/src/main/java/org/usergrid/persistence/AbstractEntity.java
@@ -268,4 +268,35 @@ public abstract class AbstractEntity implements Entity {
 		return entity;
 	}
 
+  /* (non-Javadoc)
+   * @see java.lang.Object#hashCode()
+   */
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((uuid == null) ? 0 : uuid.hashCode());
+    return result;
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#equals(java.lang.Object)
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    AbstractEntity other = (AbstractEntity) obj;
+    if (uuid == null) {
+      if (other.uuid != null)
+        return false;
+    } else if (!uuid.equals(other.uuid))
+      return false;
+    return true;
+  }
+
 }
