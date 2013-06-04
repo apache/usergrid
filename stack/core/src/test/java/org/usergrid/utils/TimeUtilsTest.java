25505b9b3554a4d2b20e8641670f5db60c335e59
zznate <zznate.m@gmail.com>
2013-06-04 16:49:42 -0500

    added additional millis and passthrough for jira time parser. For use with USERGRID-1602

--- stack/core/src/test/java/org/usergrid/utils/TimeUtilsTest.java
+++ stack/core/src/test/java/org/usergrid/utils/TimeUtilsTest.java
@@ -15,6 +15,7 @@ public class TimeUtilsTest {
     assertEquals(420000L, TimeUtils.millisFromDuration("7m"));
     assertEquals(90000L, TimeUtils.millisFromDuration("90s"));
     assertEquals(TimeUtils.millisFromDuration("1d"),TimeUtils.millisFromDuration("24h"));
+    assertEquals(250L, TimeUtils.millisFromDuration("250S"));
   }
 
   @Test
@@ -22,11 +23,18 @@ public class TimeUtilsTest {
     assertEquals(65000L, TimeUtils.millisFromDuration("1m,5s"));
     assertEquals(1293484000L, TimeUtils.millisFromDuration("14d,23h,18m,4s"));
     assertEquals(1293484000L, TimeUtils.millisFromDuration("18m,23h,4s,14d"));
+    assertEquals(8005L, TimeUtils.millisFromDuration("8s,5S"));
+    assertEquals(16137L, TimeUtils.millisFromDuration("15s,1137"));
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void meaningfulFailure() {
     TimeUtils.millisFromDuration("14z");
+  }
+
+  @Test
+  public void passThrough() {
+    assertEquals(5508L, TimeUtils.millisFromDuration("5508"));
   }
 }
 
