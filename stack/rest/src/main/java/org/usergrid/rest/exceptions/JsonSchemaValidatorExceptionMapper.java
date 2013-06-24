bec123cb0055dd995694be364abc23e34bc3718c
Ed Anuff <ed@anuff.com>
2013-06-24 15:04:44 -0700

    add validation processing report to api response

---
+++ stack/rest/src/main/java/org/usergrid/rest/exceptions/JsonSchemaValidatorExceptionMapper.java
@@ -0,0 +1,27 @@
+package org.usergrid.rest.exceptions;
+
+import static org.usergrid.utils.JsonUtils.mapToJsonString;
+
+import javax.ws.rs.core.Response;
+
+import org.usergrid.persistence.exceptions.JsonSchemaValidatorException;
+import org.usergrid.rest.ApiResponse;
+
+public class JsonSchemaValidatorExceptionMapper extends
+        AbstractExceptionMapper<JsonSchemaValidatorException> {
+
+    public Response toResponse(int status, JsonSchemaValidatorException e) {
+        logger.error("Error in request (" + status + ")", e);
+        ApiResponse response = new ApiResponse();
+        AuthErrorInfo authError = AuthErrorInfo.getForException(e);
+        if (authError != null) {
+            response.setError(authError.getType(), authError.getMessage(), e);
+        } else {
+            response.setError(e);
+        }
+        response.setValidation(e.getProcessingReport());
+        String jsonResponse = mapToJsonString(response);
+        return toResponse(status, jsonResponse);
+    }
+
+}
