424507dd4491b9caf794d3d11ace9066c3c06018
zznate <zznate.m@gmail.com>
2013-07-01 15:33:31 -0500

    Added additional checks and logging statements to provide sufficiently more detail for error diagnostics. Addresses USERGRID-1555

--- stack/core/src/main/java/org/usergrid/utils/MailUtils.java
+++ stack/core/src/main/java/org/usergrid/utils/MailUtils.java
@@ -15,59 +15,74 @@
  ******************************************************************************/
 package org.usergrid.utils;
 
-import static org.usergrid.utils.MapUtils.asProperties;
 import static org.usergrid.utils.MapUtils.filter;
 
-import java.util.Map;
 import java.util.Properties;
 
 import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.*;
 
+import com.google.common.base.Preconditions;
 import org.jsoup.Jsoup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Encapsulate the transport and error reporting of email
+ *
+ */
 public class MailUtils {
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(ConversionUtils.class);
+  private static final Logger logger = LoggerFactory
+			.getLogger(MailUtils.class);
 
-	public static Properties getMailProperties(Map<String, String> properties) {
-		return asProperties(filter(properties, "mail."));
-	}
+  private static final String LOG_PREFIX = "MAIL-DELIVERY-FAILURE - description: [%s] recipient: [%s]";
+  private static final String LOG_PREFIX_OK = "MAIL-DELIVERY-SUCCESS - recipient: [%s]";
+  private static final String CONTENT_TYPE = "Content-Type";
+  private static final String MIME_VERSION = "MIME-Version";
+  private static final String MAIL_PROP_PREFIX = "mail.";
+  private static final String PROP_TRANSPORT = MAIL_PROP_PREFIX + "transport.protocol";
 
 	public static Properties getMailProperties(Properties properties) {
-		return filter(properties, "mail.");
+		return filter(properties, MAIL_PROP_PREFIX);
 	}
 
-	public static String getHtmlMailTo(String email) {
-		return "<a href=\"mailto:" + email + "\">";
-	}
-
-	public void sendMail(String to, String from, String subject,
-			String content) {
-		sendMail(null, to, from, subject, content, null);
-	}
-
+  /**
+   * Send a message to the transport configured via the provided properties.
+   * Encapsulates the MIME multipart construction with the provided HTML
+   * and plain text forms.
+   *
+   * One of either plainText or htmlText (or both) are required. If both are null
+   * or empty we throw an IllegalArgumentException.
+   *
+   * In the case of only having htmlText, plainText is extracted from the body
+   * and wrapped in &lt;pre&gt; tags to create the multi-part.
+   *
+   * @param props
+   * @param to
+   * @param from
+   * @param subject
+   * @param plainText can be null
+   * @param htmlText
+   */
 	public void sendMail(Properties props, String to, String from,
 			String subject, String plainText, String htmlText) {
+    Preconditions.checkArgument(StringUtils.isNotBlank(htmlText) || StringUtils.isNotBlank(plainText),
+            "htmlText and plainText were both blank");
+
 		try {
 			if (props == null) {
 				props = System.getProperties();
 			}
 
-			String protocol = props.getProperty("mail.transport.protocol",
-					"smtp");
-			String host = props.getProperty("mail." + protocol + ".host");
-			String username = props.getProperty("mail." + protocol
+			String protocol = props.getProperty(PROP_TRANSPORT, "smtp");
+			String host = props.getProperty(MAIL_PROP_PREFIX + protocol + ".host");
+			String username = props.getProperty(MAIL_PROP_PREFIX + protocol
 					+ ".username");
-			String password = props.getProperty("mail." + protocol
+			String password = props.getProperty(MAIL_PROP_PREFIX + protocol
 					+ ".password");
 
 			Session session = Session.getInstance(props);
@@ -87,17 +102,17 @@ public class MailUtils {
 			if (plainText != null) {
 				MimeBodyPart plainPart = new MimeBodyPart();
 				plainPart.setContent(plainText, "text/plain");
-				plainPart.setHeader("MIME-Version", "1.0");
-				plainPart.setHeader("Content-Type",
-						"text/plain; charset=iso-8859-1");
+				plainPart.setHeader(MIME_VERSION, "1.0");
+				plainPart.setHeader(CONTENT_TYPE,
+                "text/plain; charset=iso-8859-1");
 				msgContent.addBodyPart(plainPart);
 			}
 
 			if (htmlText != null) {
 				MimeBodyPart htmlPart = new MimeBodyPart();
 				htmlPart.setContent(htmlText, "text/html");
-				htmlPart.setHeader("MIME-Version", "1.0");
-				htmlPart.setHeader("Content-Type",
+				htmlPart.setHeader(MIME_VERSION, "1.0");
+				htmlPart.setHeader(CONTENT_TYPE,
 						"text/html; charset=iso-8859-1");
 				msgContent.addBodyPart(htmlPart);
 			}
@@ -114,19 +129,33 @@ public class MailUtils {
 
 			transport.sendMessage(msg, msg.getAllRecipients());
 			transport.close();
-		} catch (Exception e) {
-			logger.error("Mail error", e);
+      logger.info(String.format(LOG_PREFIX_OK, to));
+    } catch (AddressException ae) {
+      logger.error(createErrorMessage("could not send to bad address", to), ae);
+    } catch(MessagingException me) {
+      logger.error(createErrorMessage("could not deliver message", to), me);
+    } catch (Exception e) {
+			logger.error("General exception delivering mail", to, e);
 		}
 	}
 
-	public void sendHtmlMail(String to, String from, String subject,
-			String html) {
-		sendMail(null, to, from, subject, null, html);
-	}
-
+  /**
+   * Delegates to {@link #sendMail(java.util.Properties, String, String, String, String, String)}
+   * with null as the 'plainText' argument to such.
+   *
+   * @param props
+   * @param to
+   * @param from
+   * @param subject
+   * @param html
+   */
 	public void sendHtmlMail(Properties props, String to, String from,
 			String subject, String html) {
 		sendMail(props, to, from, subject, null, html);
 	}
+
+  String createErrorMessage(String message, String toAddress) {
+    return String.format(LOG_PREFIX, message, toAddress);
+  }
 
 }
