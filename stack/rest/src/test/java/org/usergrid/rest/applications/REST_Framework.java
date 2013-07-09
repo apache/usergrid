8731e09afba1eb909fdc609cc56eafd62edc0f7c
zypher452 <greyes@apigee.com>
2013-07-09 09:56:50 -0700

    Initial Commit of Rest_Framework file
    - Added Rest_Framework file.

---
+++ stack/rest/src/test/java/org/usergrid/rest/applications/REST_Framework.java
@@ -0,0 +1,22 @@
+package org.usergrid.rest.applications;
+
+import org.junit.runner.RunWith;
+import org. junit.runners.Suite;
+
+
+
+
+/**
+ * // TODO: Document this
+ *
+ * @author ApigeeCorporation
+ * @since 4.0
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(IteratorsOmnibusTest.class)
+public class REST_Framework {
+
+
+
+}
