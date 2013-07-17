f1c3bf1ea491d77b07d7f3ae169f3868dcfe3ff9
zypher452 <greyes@apigee.com>
2013-07-16 18:49:08 -0700

    Added additional test for USERGRID-1222 to query for a username.

---
+++ stack/rest/src/test/java/org/usergrid/rest/applications/users/RetrieveUsersTest.java
@@ -0,0 +1,41 @@
+package org.usergrid.rest.applications.users;
+
+import org.codehaus.jackson.JsonNode;
+import org.junit.Test;
+import org.usergrid.rest.RestContextTest;
+import org.usergrid.rest.test.resource.CustomCollection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * // TODO: Document this
+ *
+ * @author ApigeeCorporation
+ * @since 4.0
+ */
+public class RetrieveUsersTest extends RestContextTest {
+
+  @Test //USERGRID-1222
+  public void queryForUsername() {
+    CustomCollection users = collection("users");
+
+    Map props = new HashMap();
+
+    props.put("username","Alica");
+    users.create(props);
+
+    props.put("username","Bob");
+    users.create(props);
+
+    String query = "select *";
+    String incorrectQuery = "select * where username = 'Alica'";
+
+    JsonNode incorrect = users.withQuery("select * where username = 'Alica'").get(); //users.query("select * where
+    JsonNode node = users.withQuery("select *").get(); //users.query("select *");
+
+    assertEquals(users.entityValue(query,"username",0),users.entityValue(incorrectQuery,"username",0));
+  }
+}
