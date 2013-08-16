152f6be111ae426496002c3f7e851f552481ca6a
Alex O. Karasulu <akarasulu@apache.org>
2013-08-17 01:27:50 +0300

    USERGRID-1869: Adding new tactic to simplify tests using a TestRule for an Application which for each test creates an application and has some utility methods for making it cleaner to implement tests.

--- stack/core/src/test/java/org/usergrid/persistence/CollectionIT.java
+++ stack/core/src/test/java/org/usergrid/persistence/CollectionIT.java
@@ -30,6 +30,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,6 +39,8 @@ import org.usergrid.cassandra.Concurrent;
 import org.usergrid.persistence.Results.Level;
 import org.usergrid.persistence.entities.User;
 import org.usergrid.persistence.exceptions.NoIndexException;
+import org.usergrid.Application;
+import org.usergrid.SimpleApplication;
 import org.usergrid.utils.JsonUtils;
 import org.usergrid.utils.UUIDUtils;
 
@@ -45,144 +48,136 @@ import org.usergrid.utils.UUIDUtils;
 @Concurrent()
 public class CollectionIT extends AbstractCoreIT
 {
+    private static final Logger LOG = LoggerFactory.getLogger( CollectionIT.class );
 
-  private static final Logger LOG = LoggerFactory.getLogger( CollectionIT.class );
+    @Rule
+    public Application app = new SimpleApplication( setup );
 
-  @Test
-  public void testCollection() throws Exception {
-    UUID applicationId = setup.createApplication("testOrganization", "testCollection");
-    assertNotNull(applicationId);
 
-    EntityManager em = setup.getEmf().getEntityManager(applicationId);
-    assertNotNull(em);
+    @Test
+    public void testCollection() throws Exception
+    {
+        app.add( "username", "edanuff" );
+        app.add( "email", "ed@anuff.com" );
 
-    Map<String, Object> properties = new LinkedHashMap<String, Object>();
-    properties.put("username", "edanuff");
-    properties.put("email", "ed@anuff.com");
+        Entity user = app.create( "user" );
+        assertNotNull( user );
 
-    Entity user = em.create("user", properties);
-    assertNotNull(user);
+        app.add( "actor", new LinkedHashMap<String, Object>()
+        { {
+            put( "displayName", "Ed Anuff" );
+            put( "objectType", "person" );
+        } });
+        app.add( "verb", "tweet" );
+        app.add( "content", "I ate a sammich" );
+        app.add( "ordinal", 3 );
 
-    properties = new LinkedHashMap<String, Object>();
-    properties.put("actor", new LinkedHashMap<String, Object>() {
-      {
-        put("displayName", "Ed Anuff");
-        put("objectType", "person");
-      }
-    });
-    properties.put("verb", "tweet");
-    properties.put("content", "I ate a sammich");
-    properties.put("ordinal", 3);
+        Entity activity = app.create( "activity" );
+        assertNotNull( activity );
 
-    Entity activity = em.create("activity", properties);
-    assertNotNull(activity);
+        LOG.info( "" + activity.getClass() );
+        LOG.info( JsonUtils.mapToFormattedJsonString( activity ) );
 
-    LOG.info("" + activity.getClass());
-    LOG.info(JsonUtils.mapToFormattedJsonString(activity));
+        activity = app.get( activity.getUuid() );
 
-    activity = em.get(activity.getUuid());
+        LOG.info( "Activity class = {}", activity.getClass() );
+        LOG.info( JsonUtils.mapToFormattedJsonString( activity ) );
 
-    LOG.info("" + activity.getClass());
-    LOG.info(JsonUtils.mapToFormattedJsonString(activity));
+        app.addToCollection( user, "activities", activity );
 
-    em.addToCollection(user, "activities", activity);
+        // test queries on the collection
 
-    // test queries on the collection
+        app.add( "actor", new LinkedHashMap<String, Object>()
+        { {
+            put( "displayName", "Ed Anuff" );
+            put( "objectType", "person" );
+        } });
+        app.add( "verb", "tweet2" );
+        app.add( "content", "I ate a pickle" );
+        app.add( "ordinal", 2 );
+        Entity activity2 = app.create( "activity" );
+        activity2 = app.get( activity2.getUuid() );
+        app.addToCollection( user, "activities", activity2 );
 
-    properties = new LinkedHashMap<String, Object>();
-    properties.put("actor", new LinkedHashMap<String, Object>() {
-      {
-        put("displayName", "Ed Anuff");
-        put("objectType", "person");
-      }
-    });
-    properties.put("verb", "tweet2");
-    properties.put("content", "I ate a pickle");
-    properties.put("ordinal", 2);
-    Entity activity2 = em.create("activity", properties);
-    activity2 = em.get(activity2.getUuid());
-    em.addToCollection(user, "activities", activity2);
+        app.add( "actor", new LinkedHashMap<String, Object>()
+        { {
+            put( "displayName", "Ed Anuff" );
+            put( "objectType", "person" );
+        } });
+        app.add( "verb", "tweet2" );
+        app.add( "content", "I ate an apple" );
+        app.add( "ordinal", 1 );
+        Entity activity3 = app.create( "activity" );
+        activity3 = app.get( activity3.getUuid() );
+        app.addToCollection( user, "activities", activity3 );
 
-    properties = new LinkedHashMap<String, Object>();
-    properties.put("actor", new LinkedHashMap<String, Object>() {
-      {
-        put("displayName", "Ed Anuff");
-        put("objectType", "person");
-      }
-    });
-    properties.put("verb", "tweet2");
-    properties.put("content", "I ate an apple");
-    properties.put("ordinal", 1);
-    Entity activity3 = em.create("activity", properties);
-    activity3 = em.get(activity3.getUuid());
-    em.addToCollection(user, "activities", activity3);
+        // empty query
+        Query query = new Query();
+        Results r = app.searchCollection( user, "activities", query );
+        assertEquals( 3, r.size() ); // success
 
-    // empty query
-    Query query = new Query();
-    Results r = em.searchCollection(user, "activities", query);
-    assertEquals(3, r.size()); // success
+        // query verb
+        query = new Query().addEqualityFilter( "verb", "tweet2" );
+        r = app.searchCollection( user, "activities", query );
+        assertEquals( 2, r.size() );
 
-    // query verb
-    query = new Query().addEqualityFilter("verb", "tweet2");
-    r = em.searchCollection(user, "activities", query);
-    assertEquals(2, r.size());
+        // query verb, sort created
+        query = new Query().addEqualityFilter( "verb", "tweet2" );
+        query.addSort( "created" );
+        r = app.searchCollection( user, "activities", query );
+        assertEquals( 2, r.size() );
+        List<Entity> entities = r.getEntities();
+        assertEquals( entities.get( 0 ).getUuid(), activity2.getUuid() );
+        assertEquals( entities.get( 1 ).getUuid(), activity3.getUuid() );
 
-    // query verb, sort created
-    query = new Query().addEqualityFilter("verb", "tweet2");
-    query.addSort("created");
-    r = em.searchCollection(user, "activities", query);
-    assertEquals(2, r.size());
-    List<Entity> entities = r.getEntities();
-    assertEquals(entities.get(0).getUuid(), activity2.getUuid());
-    assertEquals(entities.get(1).getUuid(), activity3.getUuid());
+        // query verb, sort ordinal
+        query = new Query().addEqualityFilter( "verb", "tweet2" );
+        query.addSort( "ordinal" );
+        r = app.searchCollection( user, "activities", query );
+        assertEquals( 2, r.size() );
+        entities = r.getEntities();
+        assertEquals( entities.get( 0 ).getUuid(), activity3.getUuid() );
+        assertEquals( entities.get( 1 ).getUuid(), activity2.getUuid() );
 
-    // query verb, sort ordinal
-    query = new Query().addEqualityFilter("verb", "tweet2");
-    query.addSort("ordinal");
-    r = em.searchCollection(user, "activities", query);
-    assertEquals(2, r.size());
-    entities = r.getEntities();
-    assertEquals(entities.get(0).getUuid(), activity3.getUuid());
-    assertEquals(entities.get(1).getUuid(), activity2.getUuid());
+        // empty query, sort content
+        query = new Query();
+        query.addSort( "content" );
+        r = app.searchCollection( user, "activities", query );
+        assertEquals( 3, r.size() );
+        entities = r.getEntities();
+        LOG.info( JsonUtils.mapToFormattedJsonString( entities ) );
+        assertEquals( entities.get( 0 ).getUuid(), activity2.getUuid() );
+        assertEquals( entities.get( 1 ).getUuid(), activity.getUuid() );
+        assertEquals( entities.get( 2 ).getUuid(), activity3.getUuid() );
 
-    // empty query, sort content
-    query = new Query();
-    query.addSort("content");
-    r = em.searchCollection(user, "activities", query);
-    assertEquals(3, r.size());
-    entities = r.getEntities();
-    LOG.info(JsonUtils.mapToFormattedJsonString(entities));
-    assertEquals(entities.get(0).getUuid(), activity2.getUuid());
-    assertEquals(entities.get(1).getUuid(), activity.getUuid());
-    assertEquals(entities.get(2).getUuid(), activity3.getUuid());
+        // empty query, sort verb
+        query = new Query();
+        query.addSort( "verb" );
+        r = app.searchCollection( user, "activities", query );
+        assertEquals( 3, r.size() );
 
-    // empty query, sort verb
-    query = new Query();
-    query.addSort("verb");
-    r = em.searchCollection(user, "activities", query);
-    assertEquals(3, r.size());
+        // empty query, sort ordinal
+        query = new Query();
+        query.addSort( "ordinal" );
+        r = app.searchCollection( user, "activities", query );
+        assertEquals( 3, r.size() );
+        entities = r.getEntities();
+        assertEquals( entities.get( 0 ).getUuid(), activity3.getUuid() );
+        assertEquals( entities.get( 1 ).getUuid(), activity2.getUuid() );
+        assertEquals( entities.get( 2 ).getUuid(), activity.getUuid() );
 
-    // empty query, sort ordinal
-    query = new Query();
-    query.addSort("ordinal");
-    r = em.searchCollection(user, "activities", query);
-    assertEquals(3, r.size());
-    entities = r.getEntities();
-    assertEquals(entities.get(0).getUuid(), activity3.getUuid());
-    assertEquals(entities.get(1).getUuid(), activity2.getUuid());
-    assertEquals(entities.get(2).getUuid(), activity.getUuid());
+        // query ordinal
+        query = new Query().addEqualityFilter( "ordinal", 2 );
+        r = app.searchCollection( user, "activities", query );
+        assertEquals( 1, r.size() );
 
-    // query ordinal
-    query = new Query().addEqualityFilter("ordinal", 2);
-    r = em.searchCollection(user, "activities", query);
-    assertEquals(1, r.size());
+        // query ordinal and sort ordinal
+        query = new Query().addEqualityFilter( "ordinal", 2 );
+        query.addSort( "ordinal" );
+        r = app.searchCollection( user, "activities", query );
+        assertEquals( 1, r.size() );
+    }
 
-    // query ordinal and sort ordinal
-    query = new Query().addEqualityFilter("ordinal", 2);
-    query.addSort("ordinal");
-    r = em.searchCollection(user, "activities", query);
-    assertEquals(1, r.size());
-  }
 
   @Test
   public void userFirstNameSearch() throws Exception {
