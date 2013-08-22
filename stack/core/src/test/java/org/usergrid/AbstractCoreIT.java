e2798a9b0907f5ba971ab7e2499816e1b8b1f2f4
Alex O. Karasulu <akarasulu@apache.org>
2013-08-22 23:17:36 +0300

    Merging from master.

--- stack/core/src/test/java/org/usergrid/AbstractCoreIT.java
+++ stack/core/src/test/java/org/usergrid/AbstractCoreIT.java
@@ -7,22 +7,14 @@ import org.slf4j.LoggerFactory;
 import org.usergrid.cassandra.Concurrent;
 import org.usergrid.mq.QueueManagerFactory;
 import org.usergrid.persistence.EntityManagerFactory;
-import org.usergrid.persistence.IndexBucketLocator;
-import org.usergrid.persistence.cassandra.CassandraService;
 import org.usergrid.utils.JsonUtils;
 
 
 @Concurrent()
 public abstract class AbstractCoreIT
 {
-    public static final boolean USE_DEFAULT_APPLICATION = false;
-
     private static final Logger LOG = LoggerFactory.getLogger( AbstractCoreIT.class );
 
-    protected EntityManagerFactory emf;
-    protected QueueManagerFactory qmf;
-    protected IndexBucketLocator indexBucketLocator;
-    protected CassandraService cassandraService;
 
     @ClassRule
     public static ITSetup setup = new ITSetup();
@@ -31,10 +23,6 @@ public abstract class AbstractCoreIT
     public AbstractCoreIT()
     {
         LOG.info("Initializing test ...");
-        emf = CoreITSuite.cassandraResource.getBean( EntityManagerFactory.class );
-        qmf = CoreITSuite.cassandraResource.getBean( QueueManagerFactory.class );
-        indexBucketLocator = CoreITSuite.cassandraResource.getBean( IndexBucketLocator.class );
-        cassandraService = CoreITSuite.cassandraResource.getBean( CassandraService.class );
     }
 
 
