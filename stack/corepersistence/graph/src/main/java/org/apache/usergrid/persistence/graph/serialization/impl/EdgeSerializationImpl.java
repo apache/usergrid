c85333757b0fe8c6757a30a3150abcb85eb6c6a9
Todd Nine <tnine@apigee.com>
2013-12-29 15:21:22 -0500

    Temp graph.  Need to test once configuration is done.

--- stack/corepersistence/graph/src/main/java/org/apache/usergrid/persistence/graph/serialization/impl/EdgeSerializationImpl.java
+++ stack/corepersistence/graph/src/main/java/org/apache/usergrid/persistence/graph/serialization/impl/EdgeSerializationImpl.java
@@ -23,25 +23,34 @@ package org.apache.usergrid.persistence.graph.serialization.impl;
 import java.nio.ByteBuffer;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.UUID;
 
-import org.apache.usergrid.persistence.collection.CollectionScope;
+import javax.inject.Inject;
+
 import org.apache.usergrid.persistence.collection.OrganizationScope;
 import org.apache.usergrid.persistence.collection.astynax.IdRowCompositeSerializer;
 import org.apache.usergrid.persistence.collection.astynax.MultiTennantColumnFamily;
 import org.apache.usergrid.persistence.collection.astynax.MultiTennantColumnFamilyDefinition;
+import org.apache.usergrid.persistence.collection.astynax.ScopedRowKey;
 import org.apache.usergrid.persistence.collection.migration.Migration;
-import org.apache.usergrid.persistence.collection.serialization.impl.CollectionScopedRowKeySerializer;
 import org.apache.usergrid.persistence.graph.Edge;
 import org.apache.usergrid.persistence.graph.SearchByEdgeType;
 import org.apache.usergrid.persistence.graph.SearchByIdType;
+import org.apache.usergrid.persistence.graph.astyanax.IdColDynamicCompositeSerializer;
+import org.apache.usergrid.persistence.graph.impl.SimpleEdge;
 import org.apache.usergrid.persistence.graph.serialization.EdgeSerialization;
+import org.apache.usergrid.persistence.graph.serialization.util.EdgeHasher;
 import org.apache.usergrid.persistence.model.entity.Id;
 
-
+import com.google.common.base.Preconditions;
 import com.google.inject.Singleton;
+import com.netflix.astyanax.Keyspace;
 import com.netflix.astyanax.MutationBatch;
+import com.netflix.astyanax.model.AbstractComposite;
+import com.netflix.astyanax.model.DynamicComposite;
 import com.netflix.astyanax.serializers.AbstractSerializer;
 import com.netflix.astyanax.serializers.StringSerializer;
+import com.netflix.astyanax.serializers.UUIDSerializer;
 
 
 /**
@@ -54,40 +63,62 @@ public class EdgeSerializationImpl implements EdgeSerialization, Migration {
 
     //row key serializers
     private static final IdRowCompositeSerializer ID_SER = IdRowCompositeSerializer.get();
-    private static final CollectionScopedRowKeySerializer<Id> ID_ROW_KEY_SER = new CollectionScopedRowKeySerializer<Id>( ID_SER );
+    private static final OrganizationScopedRowKeySerializer<Id> ID_ROW_KEY_SER =
+            new OrganizationScopedRowKeySerializer<Id>( ID_SER );
 
     private static final StringSerializer STRING_SERIALIZER = StringSerializer.get();
 
 
-    private static final MultiTennantColumnFamily<CollectionScope, Id, String> CF_SOURCE_EDGES =
-            new MultiTennantColumnFamily<CollectionScope, Id, String>( "Graph_Source_Edges", ID_ROW_KEY_SER,
+    private static final MultiTennantColumnFamily<OrganizationScope, Id, String> CF_SOURCE_EDGES =
+            new MultiTennantColumnFamily<OrganizationScope, Id, String>( "Graph_Source_Edges", ID_ROW_KEY_SER,
                     STRING_SERIALIZER );
 
 
-    private static final MultiTennantColumnFamily<CollectionScope, Id, String> CF_TARGET_EDGES =
-            new MultiTennantColumnFamily<CollectionScope, Id, String>( "Graph_Target_Edges", ID_ROW_KEY_SER,
+    private static final MultiTennantColumnFamily<OrganizationScope, Id, String> CF_TARGET_EDGES =
+            new MultiTennantColumnFamily<OrganizationScope, Id, String>( "Graph_Target_Edges", ID_ROW_KEY_SER,
                     STRING_SERIALIZER );
 
 
-    private static final MultiTennantColumnFamily<CollectionScope, Id, String> CF_SOURCE_TYPE_EDGES =
-            new MultiTennantColumnFamily<CollectionScope, Id, String>( "Graph_SourceType_Edges", ID_ROW_KEY_SER,
+    private static final MultiTennantColumnFamily<OrganizationScope, Id, String> CF_SOURCE_TYPE_EDGES =
+            new MultiTennantColumnFamily<OrganizationScope, Id, String>( "Graph_SourceType_Edges", ID_ROW_KEY_SER,
                     STRING_SERIALIZER );
 
 
-    private static final MultiTennantColumnFamily<CollectionScope, Id, String> CF_TARGET_TYPE_EDGES =
-            new MultiTennantColumnFamily<CollectionScope, Id, String>( "Graph_TargetType_Edges", ID_ROW_KEY_SER,
+    private static final MultiTennantColumnFamily<OrganizationScope, Id, String> CF_TARGET_TYPE_EDGES =
+            new MultiTennantColumnFamily<OrganizationScope, Id, String>( "Graph_TargetType_Edges", ID_ROW_KEY_SER,
                     STRING_SERIALIZER );
 
 
-    @Override
-    public Collection<MultiTennantColumnFamilyDefinition> getColumnFamilies() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    protected final Keyspace keyspace;
+
+
+    @Inject
+    public EdgeSerializationImpl( final Keyspace keyspace ) {
+        this.keyspace = keyspace;
     }
 
 
     @Override
     public MutationBatch writeEdge( final OrganizationScope scope, final Edge edge ) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        final MutationBatch batch = keyspace.prepareMutationBatch();
+
+        final String edgeTypeName = edge.getType();
+        final Id sourceNodeId = edge.getSourceNode();
+        final Id targetNodeId = edge.getTargetNode();
+
+        final long[] edgeType = EdgeHasher.createEdgeHash( edgeTypeName );
+        final long[] targetEdgeType = EdgeHasher.createEdgeHash( edgeTypeName, targetNodeId );
+        final long[] sourceEdgeType = EdgeHasher.createEdgeHash( edgeTypeName, sourceNodeId );
+
+        final ScopedRowKey<OrganizationScope, Id> sourceKey =
+                new ScopedRowKey<OrganizationScope, Id>( scope, sourceNodeId );
+        final ScopedRowKey<OrganizationScope, Id> targetKey =
+                new ScopedRowKey<OrganizationScope, Id>( scope, targetNodeId );
+
+
+        batch.withRow( CF_TARGET_EDGES, sourceKey ).putColumn();
+
+        return batch;
     }
 
 
@@ -121,38 +152,91 @@ public class EdgeSerializationImpl implements EdgeSerialization, Migration {
     }
 
 
-    private static class SourceEdgeSerializer extends AbstractSerializer<Edge> {
-
-        @Override
-        public ByteBuffer toByteBuffer( final Edge edge ) {
-            return null;  //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-
-        @Override
-        public Edge fromByteBuffer( final ByteBuffer byteBuffer ) {
-            return null;  //To change body of implemented methods use File | Settings | File Templates.
-        }
+    @Override
+    public Collection<MultiTennantColumnFamilyDefinition> getColumnFamilies() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
 
-    private static class SourceEdgeTypeSerializer extends AbstractSerializer<Edge> {
-
-        @Override
-        public ByteBuffer toByteBuffer( final Edge edge ) {
-            return null;  //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-
-        @Override
-        public Edge fromByteBuffer( final ByteBuffer byteBuffer ) {
-            return null;  //To change body of implemented methods use File | Settings | File Templates.
-        }
-    }
-
-
+    /**
+     * Serializes to a source->target edge
+     */
     private static class TargetEdgeSerializer extends AbstractSerializer<Edge> {
 
+        private static final IdColDynamicCompositeSerializer ID_COL_SERIALIZER = IdColDynamicCompositeSerializer.get();
+
+        private static final UUIDSerializer UUID_SERIALIZER = UUIDSerializer.get();
+
+
+        private final Id sourceId;
+        private final String edgeName;
+
+
+        /**
+         * Should be used for de-serializing
+         * @param sourceId
+         * @param edgeName
+         */
+        private TargetEdgeSerializer( final Id sourceId, final String edgeName ) {
+            this.sourceId = sourceId;
+            this.edgeName = edgeName;
+        }
+
+
+        /**
+         * Should be used for serialized
+         */
+        private TargetEdgeSerializer(){
+            sourceId = null;
+            edgeName = null;
+        }
+
+
+        @Override
+        public ByteBuffer toByteBuffer( final Edge edge ) {
+            final long[] targetEdgeTypes = EdgeHasher.createEdgeHash(edge.getType());
+
+
+            DynamicComposite composite = new DynamicComposite();
+
+            for ( long hash : targetEdgeTypes ) {
+                composite.add( hash );
+            }
+
+            ID_COL_SERIALIZER.toComposite( composite, edge.getTargetNode() );
+
+            composite.addComponent( edge.getVersion(), UUID_SERIALIZER );
+
+
+            return composite.serialize();
+        }
+
+
+        @Override
+        public Edge fromByteBuffer( final ByteBuffer byteBuffer ) {
+           DynamicComposite composite = DynamicComposite.fromByteBuffer( byteBuffer );
+
+            Iterator<AbstractComposite.Component<?>> iterator = composite.getComponents().iterator();
+
+            //skip the hashed data
+            Preconditions.checkArgument( iterator.hasNext(), "First hash element should be present" );
+            iterator.next();
+            Preconditions.checkArgument( iterator.hasNext(), "Second hash element should be present" );
+            iterator.next();
+
+            final Id targetId = ID_COL_SERIALIZER.fromComposite( iterator );
+
+            Preconditions.checkArgument( iterator.hasNext(), "Version element should be present" );
+
+            final UUID version = iterator.next().getValue(UUID_SERIALIZER);
+
+            return new SimpleEdge(sourceId, edgeName, targetId, version);
+        }
+    }
+
+
+    private static class TargetEdgeTypeSerializer extends AbstractSerializer<Edge> {
+
         @Override
         public ByteBuffer toByteBuffer( final Edge edge ) {
             return null;  //To change body of implemented methods use File | Settings | File Templates.
@@ -164,4 +248,6 @@ public class EdgeSerializationImpl implements EdgeSerialization, Migration {
             return null;  //To change body of implemented methods use File | Settings | File Templates.
         }
     }
+
+
 }
