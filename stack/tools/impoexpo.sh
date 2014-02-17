953e30910166ee7442388b6d3682c52767368c2a
Sungju Jin <sungju@softwaregeeks.org>
2014-02-17 21:17:16 +0900

    Adding licence headers

--- stack/tools/impoexpo.sh
+++ stack/tools/impoexpo.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # NOTE: Please define the right path to your Cassandra Home!.
 export UG_CASSANDRA_HOME=/Dev/apache-cassandra-0.8.0-rc1
 export CASSANDRA_DATA_DIR=/var/lib/cassandra
