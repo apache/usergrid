9a36362a1bb8227c693c9297fb8589f7da8c409c
Shawn Feldman <shawn.feldman@gmail.com>
2014-04-01 13:38:34 -0600

    move google analytics out of config

--- portal/dist/usergrid-portal/config.js
+++ portal/dist/usergrid-portal/config.js
@@ -1,20 +1,5 @@
 var Usergrid = Usergrid || {};
 
-//Google Analytics
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'yours']);
-try{
-    (function(document) {
-        if(!document){
-            return;
-        }
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })(document || null);
-}catch(e){};
-//End Google Analytics
-
 Usergrid.showNotifcations = true;
 
 
