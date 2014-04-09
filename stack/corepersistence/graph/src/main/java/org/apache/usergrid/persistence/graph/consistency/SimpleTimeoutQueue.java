6330206f0214a4319d7cac5f726168301aca3178
grey <greyes@apigee.com>
2014-04-09 14:10:16 -0700

    Fixed SimpleTimeoutQueue implementation.

--- stack/corepersistence/graph/src/main/java/org/apache/usergrid/persistence/graph/consistency/SimpleTimeoutQueue.java
+++ stack/corepersistence/graph/src/main/java/org/apache/usergrid/persistence/graph/consistency/SimpleTimeoutQueue.java
@@ -3,7 +3,7 @@ package org.apache.usergrid.persistence.graph.consistency;
 
 import java.util.Collection;
 
-import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClient;
 
 
 /**
@@ -11,27 +11,23 @@ import com.amazonaws.services.sqs.AmazonSQSClient;
  *
  */
 public class SimpleTimeoutQueue implements  TimeoutQueue {
+
+
     @Override
-    public <T> TimeoutEvent<T> queue( final T event, final long timeout ) {
-        /**
-         * List of steps it seems like I need to take:
-         * 1.) Create a queue
-         * 2.) Queue the event with the timeout
-         * return a element that is the queue?
-         */
-        AmazonSQSClient sqsClient;
+    public AsynchronousMessage queue( final Object event, final long timeout ) {
+        AmazonSQSAsyncClient sqsAsyncClient;
         return null;
     }
 
 
     @Override
-    public <T> Collection<TimeoutEvent<T>> take( final int maxSize, final long currentTime, final long timeout ) {
+    public Collection<AsynchronousMessage> take( final int maxSize, final long timeout ) {
         return null;
     }
 
 
     @Override
-    public <T> boolean remove( final TimeoutEvent<T> event ) {
+    public boolean remove( final AsynchronousMessage event ) {
         return false;
     }
 }
