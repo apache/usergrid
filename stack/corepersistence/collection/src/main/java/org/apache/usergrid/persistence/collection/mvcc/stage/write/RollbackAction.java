923eb4e29535626dd8750506af173e91caa50fdc
Dave Johnson <dmjohnson@apigee.com>
2014-04-23 13:19:07 -0400

    Add missing @Inject annotation, not caught in tests because we instantiate RollbackAction directly.

--- stack/corepersistence/collection/src/main/java/org/apache/usergrid/persistence/collection/mvcc/stage/write/RollbackAction.java
+++ stack/corepersistence/collection/src/main/java/org/apache/usergrid/persistence/collection/mvcc/stage/write/RollbackAction.java
@@ -17,24 +17,18 @@
  */
 package org.apache.usergrid.persistence.collection.mvcc.stage.write;
 
+import com.google.inject.Inject;
 import com.netflix.astyanax.MutationBatch;
 import com.netflix.astyanax.connectionpool.exceptions.ConnectionException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
 import org.apache.usergrid.persistence.collection.CollectionScope;
 import org.apache.usergrid.persistence.collection.exception.CollectionRuntimeException;
-import org.apache.usergrid.persistence.collection.exception.WriteUniqueVerifyException;
 import org.apache.usergrid.persistence.collection.mvcc.MvccLogEntrySerializationStrategy;
 import org.apache.usergrid.persistence.model.entity.Entity;
 import org.apache.usergrid.persistence.model.field.Field;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import rx.Observable;
 import rx.Scheduler;
 import rx.util.functions.Action1;
-import rx.functions.Func1;
-import rx.functions.FuncN;
 import rx.schedulers.Schedulers;
 
 
@@ -50,7 +44,7 @@ public class RollbackAction implements Action1<Throwable> {
     private final UniqueValueSerializationStrategy uniqueValueStrat;
     private final MvccLogEntrySerializationStrategy logEntryStrat;
 
-
+    @Inject
     public RollbackAction( 
             MvccLogEntrySerializationStrategy logEntryStrat, 
             UniqueValueSerializationStrategy uniqueValueStrat ) {
