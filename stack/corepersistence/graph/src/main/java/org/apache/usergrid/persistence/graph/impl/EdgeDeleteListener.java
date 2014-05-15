07cf40902541d851df7e52b87ba4835f79fe9444
Shawn Feldman <shawn.feldman@gmail.com>
2014-05-15 17:00:55 -0600

    fix edgedeletelistener

--- stack/corepersistence/graph/src/main/java/org/apache/usergrid/persistence/graph/impl/EdgeDeleteListener.java
+++ stack/corepersistence/graph/src/main/java/org/apache/usergrid/persistence/graph/impl/EdgeDeleteListener.java
@@ -49,21 +49,11 @@ public class EdgeDeleteListener implements MessageListener<EdgeEvent<MarkedEdge>
 
 
     @Inject
-<<<<<<< HEAD
-    public EdgeDeleteListener(  final EdgeMetadataSerialization edgeMetadataSerialization,
-                                final GraphManagerFactory graphManagerFactory, final Keyspace keyspace,
-                                @EdgeDelete final AsyncProcessor edgeDelete, final GraphFig graphFig ) {
-        this.edgeMetadataSerialization = edgeMetadataSerialization;
-        this.graphManagerFactory = graphManagerFactory;
-        this.keyspace = keyspace;
-        this.graphFig = graphFig;
-=======
     public EdgeDeleteListener( @EdgeDelete final AsyncProcessor edgeDelete, final EdgeDeleteRepair edgeDeleteRepair,
                                final EdgeMetaRepair edgeMetaRepair ) {
 
         this.edgeDeleteRepair = edgeDeleteRepair;
         this.edgeMetaRepair = edgeMetaRepair;
->>>>>>> 74f95ac4c39c6d2e48dcee56d45a5b69d9b4ea5e
 
         edgeDelete.addListener( this );
     }
@@ -75,129 +65,7 @@ public class EdgeDeleteListener implements MessageListener<EdgeEvent<MarkedEdge>
         final MarkedEdge edge = delete.getData();
         final OrganizationScope scope = delete.getOrganizationScope();
         final UUID maxVersion = edge.getVersion();
-<<<<<<< HEAD
-        final GraphManager graphManager = graphManagerFactory.createEdgeManager( scope );
 
-
-        return Observable.from( edge ).flatMap( new Func1<Edge, Observable<MutationBatch>>() {
-                                                    @Override
-                                                    public Observable<MutationBatch> call( final Edge edge ) {
-
-                                                        final MutationBatch batch = keyspace.prepareMutationBatch();
-
-
-                                                        //search by edge type and target type.  If any other edges with this target type exist,
-                                                        // we can't delete it
-                                                        Observable<Integer> sourceIdType = graphManager.loadEdgesFromSourceByType(
-                                                                new SimpleSearchByIdType( edge.getSourceNode(), edge.getType(), maxVersion,
-                                                                        edge.getTargetNode().getType(), null ) ).take( 2 ).count()
-                                                                .doOnNext( new Action1<Integer>() {
-                                                                    @Override
-                                                                    public void call( final Integer count ) {
-                                                                        //There's nothing to do,
-                                                                        // we have 2 different edges with the
-                                                                        // same edge type and
-                                                                        // target type.  Don't delete meta data
-                                                                        if ( count == 1 ) {
-                                                                            final MutationBatch delete =
-                                                                                    edgeMetadataSerialization
-                                                                                            .removeEdgeTypeFromSource(
-                                                                                                    scope, edge );
-                                                                            batch.mergeShallow( delete );
-                                                                        }
-                                                                    }
-                                                                } );
-
-
-                                                        Observable<Integer> targetIdType = graphManager.loadEdgesToTargetByType(
-                                                                new SimpleSearchByIdType( edge.getTargetNode(), edge.getType(), maxVersion,
-                                                                        edge.getSourceNode().getType(), null ) ).take( 2 ).count()
-                                                                .doOnNext( new Action1<Integer>() {
-                                                                    @Override
-                                                                    public void call( final Integer count ) {
-                                                                        //There's nothing to do,
-                                                                        // we have 2 different edges with the
-                                                                        // same edge type and
-                                                                        // target type.  Don't delete meta data
-                                                                        if ( count == 1 ) {
-                                                                            final MutationBatch delete =
-                                                                                    edgeMetadataSerialization
-                                                                                            .removeEdgeTypeToTarget(
-                                                                                                    scope, edge );
-                                                                            batch.mergeShallow( delete );
-                                                                        }
-                                                                    }
-                                                                } );
-
-
-                                                        //search by edge type and target type.  If any other edges with this target type exist,
-                                                        // we can't delete it
-                                                        Observable<Integer> sourceType = graphManager.loadEdgesFromSource(
-                                                                new SimpleSearchByEdgeType( edge.getSourceNode(), edge.getType(), maxVersion, null ) ).take( 2 )
-                                                                .count().doOnNext( new Action1<Integer>() {
-                                                                    @Override
-                                                                    public void call( final Integer count ) {
-                                                                        //There's nothing to do,
-                                                                        // we have 2 different edges with the
-                                                                        // same edge type and
-                                                                        // target type.  Don't delete meta data
-                                                                        if ( count == 1 ) {
-                                                                            final MutationBatch delete =
-                                                                                    edgeMetadataSerialization.removeEdgeTypeFromSource( scope, edge );
-                                                                            batch.mergeShallow( delete );
-                                                                        }
-                                                                    }
-                                                                } );
-
-
-                                                        Observable<Integer> targetType = graphManager.loadEdgesToTarget(
-                                                                new SimpleSearchByEdgeType( edge.getTargetNode(), edge.getType(), maxVersion, null ) ).take( 2 )
-                                                                .count().doOnNext( new Action1<Integer>() {
-                                                                    @Override
-                                                                    public void call( final Integer count ) {
-                                                                        //There's nothing to do,
-                                                                        // we have 2 different edges with the
-                                                                        // same edge type and
-                                                                        // target type.  Don't delete meta data
-                                                                        if ( count == 1 ) {
-                                                                            final MutationBatch delete =
-                                                                                    edgeMetadataSerialization.removeEdgeTypeToTarget( scope, edge );
-                                                                            batch.mergeShallow( delete );
-                                                                        }
-                                                                    }
-                                                                } );
-
-
-                                                        //no op, just wait for each observable to populate the mutation before returning it
-                                                        return Observable.zip(sourceIdType, targetIdType, sourceType, targetType,
-                                                                new Func4<Integer, Integer, Integer, Integer, MutationBatch>() {
-                                                                    @Override
-                                                                    public MutationBatch call( final Integer integer,
-                                                                                               final Integer integer2, final Integer integer3,
-                                                                                               final Integer integer4 ) {
-                                                                        return batch;
-                                                                    }
-                                                                } );
-                                                    }
-                                                }
-
-
-        ).map( new Func1<MutationBatch, EdgeEvent<Edge>>() {
-            @Override
-            public EdgeEvent<Edge> call( final MutationBatch mutationBatch ) {
-
-                //actually delete the edge from both the commit log and
-                try {
-                    mutationBatch.execute();
-                }
-                catch ( ConnectionException e ) {
-                    throw new RuntimeException( "Unable to execute mutation", e );
-                }
-
-                return delete;
-            }
-        } );
-=======
 
         return edgeDeleteRepair.repair( scope, edge, delete.getTimestamp() )
 
@@ -228,6 +96,5 @@ public class EdgeDeleteListener implements MessageListener<EdgeEvent<MarkedEdge>
                         return delete;
                     }
                 } );
->>>>>>> 74f95ac4c39c6d2e48dcee56d45a5b69d9b4ea5e
     }
 }
