ccad5826164a0485f10a0f5ee145668d1e88b85e
grey <greyes@apigee.com>
2014-05-27 13:34:22 -0700

    Cleaned up the file to make it clearer what it is accomplishing.

--- stack/corepersistence/collection/src/main/java/org/apache/usergrid/persistence/collection/util/RepairUtil.java
+++ stack/corepersistence/collection/src/main/java/org/apache/usergrid/persistence/collection/util/RepairUtil.java
@@ -12,9 +12,6 @@ import org.apache.usergrid.persistence.collection.mvcc.changelog.ChangeLogEntry;
 import org.apache.usergrid.persistence.collection.mvcc.changelog.ChangeLogGenerator;
 import org.apache.usergrid.persistence.collection.mvcc.changelog.ChangeLogGeneratorImpl;
 import org.apache.usergrid.persistence.collection.mvcc.entity.MvccEntity;
-import org.apache.usergrid.persistence.model.entity.Entity;
-
-import com.google.common.base.Optional;
 
 
 /**
@@ -23,11 +20,10 @@ import com.google.common.base.Optional;
  */
 public class RepairUtil {
 
-    private static  ChangeLogGenerator changeLogGenerator = new ChangeLogGeneratorImpl();
+    private static final ChangeLogGenerator changeLogGenerator = new ChangeLogGeneratorImpl();
 
-    public static MvccEntity  repair(Iterator<MvccEntity> results,
-                                     CollectionScope collectionScope,
-                                     MvccEntitySerializationStrategy entitySerializationStrategy){
+    public static MvccEntity repair( Iterator<MvccEntity> results, CollectionScope collectionScope,
+                                     MvccEntitySerializationStrategy entitySerializationStrategy ) {
 
         //nothing to do, we didn't get a result back
         if ( !results.hasNext() ) {
@@ -35,59 +31,53 @@ public class RepairUtil {
         }
 
         MvccEntity mvccEntity = results.next();
-        final Optional<Entity> targetVersion = mvccEntity.getEntity();
         List<MvccEntity> partialEntities = new ArrayList<>();
 
         //this entity has been marked as cleared.(deleted)
         //The version exists, but does not have entity data
-        if ( !targetVersion.isPresent() && (mvccEntity.getStatus() == MvccEntity.Status.DELETED
-                                            || mvccEntity.getStatus() == MvccEntity.Status.COMPLETE)) {
+        if ( !mvccEntity.getEntity().isPresent() && ( mvccEntity.getStatus() == MvccEntity.Status.DELETED
+                || mvccEntity.getStatus() == MvccEntity.Status.COMPLETE ) ) {
             return null;
         }
 
-        //TODO: make this return a mvcc entity?
         if ( mvccEntity.getStatus() == MvccEntity.Status.COMPLETE ) {
             return mvccEntity;
         }
         partialEntities.add( mvccEntity );
 
 
-        while(results.hasNext()){
+        while ( results.hasNext() ) {
             mvccEntity = results.next();
             partialEntities.add( mvccEntity );
-            if(mvccEntity.getStatus() == MvccEntity.Status.PARTIAL){
-                continue;
+
+            if ( mvccEntity.getStatus() != MvccEntity.Status.PARTIAL ) {
+                return repairAndWrite( partialEntities, mvccEntity, entitySerializationStrategy, collectionScope );
             }
-            else{
-
-                //reverse the list
-                Collections.reverse(partialEntities);
-
-                //
-                List<ChangeLogEntry> chgPersist = new ArrayList<>();
-                //Create a sublist of 2 containing completed entity and partial entity
-                List<MvccEntity> subEntList =  new ArrayList<>(  );
-
-                chgPersist = changeLogGenerator.getChangeLog( partialEntities.iterator(),
-                        partialEntities.get( partialEntities.size()-1 ).getVersion());
-
-                mvccEntity =  entityRepair( chgPersist,partialEntities,mvccEntity );
-
-                try {
-                    entitySerializationStrategy.write( collectionScope, mvccEntity).execute();
-                }
-                catch ( Exception e ) {
-                    throw new RuntimeException( "Couldn't rewrite repaired entity", e );
-                }
-                return mvccEntity;
-
-            }
-
         }
         return null;
     }
 
-    //TODO: change the list to be an iterator
+
+    private static MvccEntity repairAndWrite( List<MvccEntity> partialEntities, MvccEntity mvccEntity,
+                                              MvccEntitySerializationStrategy entitySerializationStrategy,
+                                              CollectionScope collectionScope ) {
+        Collections.reverse( partialEntities );
+
+        //repair
+        mvccEntity = entityRepair( changeLogGenerator.getChangeLog( partialEntities.iterator(),
+                        partialEntities.get( partialEntities.size() - 1 ).getVersion() ), partialEntities, mvccEntity
+                                 );
+
+        try {
+            entitySerializationStrategy.write( collectionScope, mvccEntity ).execute();
+        }
+        catch ( Exception e ) {
+            throw new RuntimeException( "Couldn't rewrite repaired entity", e );
+        }
+        return mvccEntity;
+    }
+
+
     private static MvccEntity entityRepair( List<ChangeLogEntry> changeLogEntryList, List<MvccEntity> results,
                                             MvccEntity completedEntity ) {
         int changeLogIndex = 0;
@@ -104,7 +94,7 @@ public class RepairUtil {
                     if ( changeType.equals( ChangeLogEntry.ChangeType.PROPERTY_DELETE ) ) {
                         completedEntity.getEntity().get().getFields().remove( changeLogEntry.getField() );
                     }
-                    else if ( changeType.equals( ChangeLogEntry.ChangeType.PROPERTY_WRITE) ) {
+                    else if ( changeType.equals( ChangeLogEntry.ChangeType.PROPERTY_WRITE ) ) {
                         completedEntity.getEntity().get().setField( changeLogEntry.getField() );
                     }
                     changeLogIndex++;
@@ -115,7 +105,7 @@ public class RepairUtil {
             }
         }
 
-        if(!completedEntity.getEntity().isPresent()){
+        if ( !completedEntity.getEntity().isPresent() ) {
             return null;
         }
 
