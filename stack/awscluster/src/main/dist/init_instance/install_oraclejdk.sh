92e6b5a2c90fcdeead65fcdb341f654ac9daae8e
grey <greyes@apigee.com>
2014-07-07 15:47:42 -0700

    Fix for failing java sdk copying.

--- stack/awscluster/src/main/dist/init_instance/install_oraclejdk.sh
+++ stack/awscluster/src/main/dist/init_instance/install_oraclejdk.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 pushd /tmp
 
-# 
+#
 #  Licensed to the Apache Software Foundation (ASF) under one or more
 #   contributor license agreements.  The ASF licenses this file to You
 #  under the Apache License, Version 2.0 (the "License"); you may not
 #  use this file except in compliance with the License.
 #  You may obtain a copy of the License at
-# 
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,18 +29,18 @@ echo ${RELEASE_BUCKET}
 # Get JDK from the release bucket
 s3cmd --config=/etc/s3cfg get s3://${RELEASE_BUCKET}/jdk-7u45-linux-x64.gz
 
-# Install it as they do here: 
+# Install it as they do here:
 # http://askubuntu.com/questions/56104/how-can-i-install-sun-oracles-proprietary-java-6-7-jre-or-jdk
 tar -xvf jdk-7u45-linux-x64.gz
 mkdir -p /usr/lib/jvm
-mv ./jdk1.7.0_45 /usr/lib/jvm/jdk1.7.0
+mv ./jdk1.7.0_60 /usr/lib/jvm/jdk1.7.0
 
 update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.7.0/bin/java" 2000
 update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.7.0/bin/javac" 2000
 update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.7.0/bin/javaws" 2000
 
-chmod a+x /usr/bin/java 
-chmod a+x /usr/bin/javac 
+chmod a+x /usr/bin/java
+chmod a+x /usr/bin/javac
 chmod a+x /usr/bin/javaws
 chown -R root:root /usr/lib/jvm/jdk1.7.0
 
