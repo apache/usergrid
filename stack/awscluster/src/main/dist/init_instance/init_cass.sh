adfac04ee66ea376f52af260655e435f20d12581
Todd Nine <toddnine@apache.org>
2014-07-28 09:58:55 -0600

    Added missing files  for setup

---
+++ stack/awscluster/src/main/dist/init_instance/init_cass.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+echo "${HOSTNAME}" > /etc/hostname
+echo "127.0.0.1 ${HOSTNAME}" >> /etc/hosts
+hostname `cat /etc/hostname`
+
+echo "US/Eastern" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
+
+PKGS="openjdk-7-jdk s3cmd ntp unzip groovy"
+apt-get update
+apt-get -y install ${PKGS}
+
+# Install AWS Java SDK and get it into the Groovy classpath
+curl http://sdk-for-java.amazonwebservices.com/latest/aws-java-sdk.zip > /tmp/aws-sdk-java.zip
+cd /usr/share/
+unzip /tmp/aws-sdk-java.zip 
+mkdir -p /home/ubuntu/.groovy/lib
+cp /usr/share/aws-java-sdk-*/third-party/*/*.jar /home/ubuntu/.groovy/lib
+cp /usr/share/aws-java-sdk-*/lib/* /home/ubuntu/.groovy/lib 
+# except for evil stax
+rm /home/ubuntu/.groovy/lib/stax*
+ln -s /home/ubuntu/.groovy /root/.groovy
+
+# Build environment for Groovy scripts
+. /etc/profile.d/aws-credentials.sh
+. /etc/profile.d/usergrid-env.sh
+
+
+
+# Register as a Cassandra node and wait for enough other servers to join
+cd /usr/share/usergrid/scripts
+
+groovy registry_register.groovy
+groovy wait_for_cassandra.groovy
+
+# Install and stop Cassandra so we can configure it
+cd /etc/apt/sources.list.d
+cat >> cassandra.sources.list << EOF
+deb http://www.apache.org/dist/cassandra/debian 12x main
+EOF
+sudo apt-get update
+sudo apt-get -y install libcap2
+sudo apt-get --force-yes -y install cassandra
+/etc/init.d/cassandra stop
+
+cd /usr/share/usergrid/init_instance
+cd /usr/share/usergrid/scripts
+groovy configure_cassandra.groovy > /etc/cassandra/cassandra.yaml
+
+# Go
+/etc/init.d/cassandra start
+
