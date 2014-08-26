bd8fca865e9288e088a13a7edd7f2a1682d96e77
Dave Johnson <snoopdave@apache.org>
2014-08-26 08:26:02 -0400

    Add ASF header to many files.

--- release/sign-release.sh
+++ release/sign-release.sh
@@ -1,6 +1,10 @@
-export rcstring=""
+
+
+export rcstring="-rc2"
 export vstring="1.0.0"
 
-gpg --armor --detach-sig target/usergrid-release-${vstring}${rcstring}-source.zip
-gpg --armor --detach-sig target/usergrid-release-${vstring}${rcstring}-source.tar.gz
+gpg --armor --detach-sig target/apache-usergrid-incubating-${vstring}${rcstring}-source.zip
+gpg --armor --detach-sig target/apache-usergrid-incubating-${vstring}${rcstring}-source.tar.gz
 
+gpg --print-md MD5 apache-usergrid-incubating-${vstring}${rcstring}-source.zip > apache-usergrid-incubating-${vstring}${rcstring}-source.zip.md5
+gpg --print-md MD5 apache-usergrid-incubating-${vstring}${rcstring}-source.tar.gz > apache-usergrid-incubating-${vstring}${rcstring}-source.tar.gz.md5
