ac39bf270c5af3bf6191c1f6fbb4c2cf74d3dff9
Shawn Feldman <sfeldman@apache.org>
2014-09-17 15:10:34 -0600

    try to remove graphite errors

--- stack/core/src/main/java/org/apache/usergrid/metrics/MetricsFactory.java
+++ stack/core/src/main/java/org/apache/usergrid/metrics/MetricsFactory.java
@@ -51,18 +51,18 @@ public class MetricsFactory {
         registry = new MetricRegistry();
         String badHost = "badhost";
         String metricsHost = properties.getProperty("usergrid.metrics.graphite.host", badHost);
-        Graphite graphite = new Graphite(new InetSocketAddress(metricsHost, 2003));
-        graphiteReporter = GraphiteReporter.forRegistry(registry)
-                .prefixedWith("notifications")
-                .convertRatesTo(TimeUnit.SECONDS)
-                .convertDurationsTo(TimeUnit.MILLISECONDS)
-                .filter(MetricFilter.ALL)
-                .build(graphite);
+
         if(metricsHost!=badHost) {
+            Graphite graphite = new Graphite(new InetSocketAddress(metricsHost, 2003));
+            graphiteReporter = GraphiteReporter.forRegistry(registry)
+                    .prefixedWith("notifications")
+                    .convertRatesTo(TimeUnit.SECONDS)
+                    .convertDurationsTo(TimeUnit.MILLISECONDS)
+                    .filter(MetricFilter.ALL)
+                    .build(graphite);
             graphiteReporter.start(30, TimeUnit.SECONDS);
         }else {
             LOG.warn("MetricsService:Logger not started.");
-            graphiteReporter.stop();
         }
         hashMap = new ConcurrentHashMap<String, Metric>();
 
