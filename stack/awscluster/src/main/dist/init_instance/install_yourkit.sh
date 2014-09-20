a6d9fedacc25aae8be1265ac3104df005e6fb62b
Todd Nine <toddnine@apache.org>
2014-09-20 16:56:22 -0600

    Fixes data dir for info

--- stack/awscluster/src/main/dist/init_instance/install_yourkit.sh
+++ stack/awscluster/src/main/dist/init_instance/install_yourkit.sh
@@ -28,12 +28,12 @@ cd /tmp/yourkit
 s3cmd --config=/etc/s3cfg get s3://${RELEASE_BUCKET}/yjp-2014-build-14104.zip
 unzip /tmp/yourkit/yjp-2014-build-14104.zip
 
-mkdir -p /tmp/yourkitreports
+mkdir -p /mnt/yourkitreports
 
-chown -R tomcat7.tomcat7 /tmp/yourkitreports
+chown -R tomcat7.tomcat7 /mnt/yourkitreports
 
 cat >> /etc/default/tomcat7 << EOF
-JAVA_OPTS="${JAVA_OPTS} -agentpath:/tmp/yourkit/yjp-2014-build-14104/bin/linux-x86-64/libyjpagent.so=port=10001,logdir=/tmp/yourkitreports,onexit=snapshot"
+JAVA_OPTS="${JAVA_OPTS} -agentpath:/tmp/yourkit/yjp-2014-build-14104/bin/linux-x86-64/libyjpagent.so=port=10001,logdir=/mnt/yourkitreports,dir=/mnt/yourkitreports,onexit=snapshot"
 EOF
 
 fi
