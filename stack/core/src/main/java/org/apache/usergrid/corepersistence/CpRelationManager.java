e4782769323dc39cef4304b00769630e1ee85dd2
Dave Johnson <dmjohnson@apigee.com>
2014-09-22 10:03:16 -0400

    Optimization in addToCollection() methods to prevent two unnecessary Cassandra reads.

--- stack/core/src/main/java/org/apache/usergrid/corepersistence/CpRelationManager.java
+++ stack/core/src/main/java/org/apache/usergrid/corepersistence/CpRelationManager.java
@@ -635,7 +635,13 @@ public class CpRelationManager implements RelationManager {
 
     public Entity addToCollection(String collName, EntityRef itemRef, boolean connectBack ) throws Exception {
 
-        Entity itemEntity = em.get( itemRef );
+        // don't fetch entity if we've already got one
+        final Entity itemEntity;
+        if ( itemRef instanceof Entity ) {
+            itemEntity = (Entity)itemRef;
+        } else {
+            itemEntity = em.get( itemRef );
+        }
 
         if ( itemEntity == null ) {
             return null;
@@ -682,6 +688,7 @@ public class CpRelationManager implements RelationManager {
                 itemRef.getType(), itemRef.getUuid() });
         UUID timeStampUuid =   memberEntity.getId().getUuid() != null &&  UUIDUtils.isTimeBased( memberEntity.getId().getUuid()) ?  memberEntity.getId().getUuid() : UUIDUtils.newTimeUUID();
         long uuidHash =    UUIDUtils.getUUIDLong(timeStampUuid);
+
         // create graph edge connection from head entity to member entity
         Edge edge = new SimpleEdge(
             cpHeadEntity.getId(),
