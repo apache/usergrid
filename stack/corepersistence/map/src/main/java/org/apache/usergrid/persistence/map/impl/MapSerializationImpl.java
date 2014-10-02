b749a7a8bdda67d2859b675cb3cb6737b0137d18
Shawn Feldman <sfeldman@apache.org>
2014-10-02 15:05:21 -0600

    moving headers

--- stack/corepersistence/map/src/main/java/org/apache/usergrid/persistence/map/impl/MapSerializationImpl.java
+++ stack/corepersistence/map/src/main/java/org/apache/usergrid/persistence/map/impl/MapSerializationImpl.java
@@ -1,4 +1,4 @@
-package org.apache.usergrid.persistence.map.impl;/*
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -17,7 +17,7 @@ package org.apache.usergrid.persistence.map.impl;/*
  * under the License.
  */
 
-
+package org.apache.usergrid.persistence.map.impl;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.UUID;
