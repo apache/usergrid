cb6647a53154dbfd5894f2b031f0941b9d9e6dc3
Todd Nine <tnine@apigee.com>
2014-11-03 16:20:34 -0700

    Fixed package declaration issue

--- stack/loadtests/src/main/scala/org/apache/usergrid/settings/Settings.scala
+++ stack/loadtests/src/main/scala/org/apache/usergrid/settings/Settings.scala
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- package org.apache.usergrid.settings
+package org.apache.usergrid.settings
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
