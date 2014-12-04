c4973e939b580f973f40b6991e5d1b1dac204181
Dave Johnson <dmjohnson@apigee.com>
2014-12-04 17:38:14 -0500

    Minor formatting changes only.

--- stack/core/src/test/java/org/apache/usergrid/persistence/GeoIT.java
+++ stack/core/src/test/java/org/apache/usergrid/persistence/GeoIT.java
@@ -17,20 +17,22 @@
 package org.apache.usergrid.persistence;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import org.apache.usergrid.AbstractCoreIT;
 import org.apache.usergrid.cassandra.Concurrent;
 import org.apache.usergrid.persistence.geo.model.Point;
 import org.apache.usergrid.persistence.index.query.Query;
 import org.apache.usergrid.persistence.model.field.value.Location;
 import org.apache.usergrid.utils.MapUtils;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.*;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 
 @Concurrent()
@@ -39,11 +41,13 @@ public class GeoIT extends AbstractCoreIT {
 
     int NEARBY_RADIUS = 10000;
     int CIRCUMFERENCE_OF_THE_EARTH = 40000000;
+
     /*
       A list of concrete entities with locations to be used for geoQuery tests
       NOTE: Adding or removing items from this list could affect test outcome!!!
      */
-    private static List<Map<String, Object>> LOCATION_PROPERTIES = new ArrayList<Map<String, Object>>();
+    private static List<Map<String, Object>> LOCATION_PROPERTIES = 
+            new ArrayList<Map<String, Object>>();
 
     static {
         LOCATION_PROPERTIES.add(new LinkedHashMap<String, Object>() {{
@@ -194,11 +198,17 @@ public class GeoIT extends AbstractCoreIT {
         em.refreshIndex();
         //4. Test that the user is within 2000m of the entity
         Results emSearchResults = em.searchConnectedEntities(user,
-            Query.fromQL("location within 2000 of " + ((LinkedHashMap<String, Object>) userProperties.get("location")).get("latitude") + ", " + ((LinkedHashMap<String, Object>) userProperties.get("location")).get("longitude")).setConnectionType("likes"));
+            Query.fromQL("location within 2000 of " 
+                + ((LinkedHashMap<String, Object>) userProperties.get("location")).get("latitude") 
+                + ", " + ((LinkedHashMap<String, Object>) 
+                        userProperties.get("location")).get("longitude")).setConnectionType("likes"));
         assertEquals(1, emSearchResults.size());
         //5. Test that the user is NOT within 1000m of the entity
         emSearchResults = em.searchConnectedEntities(user,
-            Query.fromQL("location within 1000 of " + ((LinkedHashMap<String, Object>) userProperties.get("location")).get("latitude") + ", " + ((LinkedHashMap<String, Object>) userProperties.get("location")).get("longitude")).setConnectionType("likes"));
+            Query.fromQL("location within 1000 of " 
+                + ((LinkedHashMap<String, Object>) userProperties.get("location")).get("latitude") 
+                + ", " + ((LinkedHashMap<String, Object>) 
+                        userProperties.get("location")).get("longitude")).setConnectionType("likes"));
         assertEquals(0, emSearchResults.size());
         //cleanup
         em.delete(user);
@@ -263,7 +273,8 @@ public class GeoIT extends AbstractCoreIT {
             + center.getLat() + "," + center.getLon());
         listResults = em.searchCollection(em.getApplicationRef(), "stores", query2);
 
-        assertEquals("Results within " + CIRCUMFERENCE_OF_THE_EARTH + "m from center", LOCATION_PROPERTIES.size(), listResults.size());
+        assertEquals("Results within " + CIRCUMFERENCE_OF_THE_EARTH 
+                + "m from center", LOCATION_PROPERTIES.size(), listResults.size());
 
     }
 
@@ -297,7 +308,8 @@ public class GeoIT extends AbstractCoreIT {
         query = Query.fromQL("select * where location within " + CIRCUMFERENCE_OF_THE_EARTH + " of "
             + center.getLat() + "," + center.getLon());
         listResults = em.searchCollection(em.getApplicationRef(), "stores", query);
-        assertEquals("Results within " + CIRCUMFERENCE_OF_THE_EARTH + "m from center", LOCATION_PROPERTIES.size(), listResults.size());
+        assertEquals("Results within " + CIRCUMFERENCE_OF_THE_EARTH 
+                + "m from center", LOCATION_PROPERTIES.size(), listResults.size());
     }
 
     /**
@@ -338,7 +350,8 @@ public class GeoIT extends AbstractCoreIT {
             Query query2 = Query.fromQL("select * where location within 40000000 of "
                 + center.getLat() + "," + center.getLon());
             listResults = em.searchCollection(em.getApplicationRef(), "stores", query2);
-            assertEquals("Results from center point to ridiculously far", LOCATION_PROPERTIES.size(), listResults.size());
+            assertEquals("Results from center point to ridiculously far", 
+                    LOCATION_PROPERTIES.size(), listResults.size());
         }
     }
 
@@ -541,7 +554,8 @@ public class GeoIT extends AbstractCoreIT {
         //    String queryString = String.format("select * where location within 100 of 0,
         // 0 and index >= %d and index < %d order by index",min, max);
 
-        String queryString = String.format("select * where index >= %d and index < %d order by index", min, max);
+        String queryString = String.format(
+                "select * where index >= %d and index < %d order by index", min, max);
 
         Query query = Query.fromQL(queryString);
 
@@ -588,7 +602,8 @@ public class GeoIT extends AbstractCoreIT {
             float latitude = minLatitude + latitudeDelta * i;
             float longitude = minLongitude + longitudeDelta * i;
 
-            Map<String, Float> location = MapUtils.hashMap("latitude", latitude).map("longitude", longitude);
+            Map<String, Float> location = 
+                    MapUtils.hashMap("latitude", latitude).map("longitude", longitude);
 
             Map<String, Object> data = new HashMap<String, Object>(2);
             data.put("name", String.valueOf(i));
