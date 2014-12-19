ac58b2cd95d0bd797dce3d96c9db6f23e844570d
ryan bridges <rbridges@apigee.com>
2014-12-19 18:45:42 -0500

    [USERGRID-288]Updated Geo tests to work with the new test framework

--- stack/rest/src/test/java/org/apache/usergrid/rest/applications/queries/BasicGeoTests.java
+++ stack/rest/src/test/java/org/apache/usergrid/rest/applications/queries/BasicGeoTests.java
@@ -17,32 +17,17 @@
 package org.apache.usergrid.rest.applications.queries;
 
 
-import java.util.*;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.sun.jersey.api.client.UniformInterfaceException;
-
-import java.io.IOException;
-
-import org.apache.usergrid.persistence.Entity;
-import org.apache.usergrid.persistence.EntityManager;
-import org.apache.usergrid.persistence.Results;
-
-import org.jclouds.json.Json;
-import org.junit.Rule;
+import org.apache.usergrid.rest.test.resource2point0.AbstractRestIT;
+import org.apache.usergrid.rest.test.resource2point0.model.Entity;
+import org.apache.usergrid.utils.MapUtils;
 import org.junit.Test;
 
-import org.apache.usergrid.persistence.geo.model.Point;
-import org.apache.usergrid.persistence.index.query.Query;
-import org.apache.usergrid.persistence.index.utils.UUIDUtils;
-import org.apache.usergrid.rest.AbstractRestIT;
-import org.apache.usergrid.rest.TestContextSetup;
-import org.apache.usergrid.rest.test.resource.CustomCollection;
+import java.io.IOException;
+import java.util.HashMap;
 
-import static org.junit.Assert.assertEquals;
-import static org.apache.usergrid.utils.MapUtils.hashMap;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 
 /**
@@ -52,217 +37,197 @@ import static org.junit.Assert.fail;
  */
 public class BasicGeoTests extends AbstractRestIT {
 
-    @Rule
-    public TestContextSetup context = new TestContextSetup( this );
+  public final String latitude = "latitude";
 
-    public final String latitude = "latitude";
+  /**
+   * Create a entity with a geo location point in it
+   * 1. Create entity
+   * 2. Verify that the entity was created
+   */
+  @Test
+  public void createEntityWithGeoLocationPoint() throws IOException {
 
-    /**
-     * Create a entity with a geo location point in it
-     * 1. Create entity
-     * 2. Verify that the entity was created
-     */
-    @Test
-    public void createEntityWithGeoLocationPoint() throws IOException {
+    Double lat = 37.776753;
+    Double lon = -122.407846;
 
-        String collectionType = "stores";
-        JsonNode node = null;
-        Double lat = 37.776753;
-        Double lon = -122.407846;
-        //1. Create entity
-        Map<String, Double> latLon = hashMap("latitude", lat);
-        latLon.put( "longitude", lon );
-        Map<String, Object> entityData = new HashMap<String, Object>();
-        entityData.put( "location", latLon );
+    //1. Create entity
+    Entity entity = new Entity();
+    entity.put("location", new MapUtils.HashMapBuilder<String, Double>()
+        .map("latitude", lat)
+        .map("longitude", lon));
 
-        try {
-            node = context.collection( collectionType ).post( entityData );
-        }
-        catch ( UniformInterfaceException e ) {
-            JsonNode nodeError = mapper.readTree( e.getResponse().getEntity( String.class ) );
-            fail( nodeError.get( "error" ).textValue() );
-        }
+    Entity testEntity = this.app().collection("stores").post(entity);
 
-        //2. Verify that the entity was created
-        assertNotNull( node );
-        assertEquals( lat.toString(), node.get("location").get("latitude").asText() );
-        assertEquals( lon.toString(), node.get( "location" ).get("longitude").asText() );
+    //2. Verify that the entity was created
+    assertNotNull(testEntity);
+    assertEquals(lat.toString(), ((HashMap<String, Object>) testEntity.get("location")).get("latitude").toString());
+    assertEquals(lon.toString(), ((HashMap<String, Object>) testEntity.get("location")).get("longitude").toString());
 
-    }
+  }
 
-    /**
-     * Update an entity with a geo location point in it
-     * 1. create an entity with a geo point
-     * 2. read back that entity make sure it is accurate
-     * 3. update the geo point to a new value
-     * 4. read back the updated entity, make sure it is accurate
-     */
-    @Test
-    public void updateEntityWithGeoLocationPoint() throws IOException {
+  /**
+   * Update an entity with a geo location point in it
+   * 1. create an entity with a geo point
+   * 2. read back that entity make sure it is accurate
+   * 3. update the geo point to a new value
+   * 4. read back the updated entity, make sure it is accurate
+   */
+  @Test
+  public void updateEntityWithGeoLocationPoint() throws IOException {
 
-        String collectionType = "stores";
-        String entityName = "cornerStore";
-        JsonNode entity = null;
-        Double lat = 37.776753;
-        Double lon = -122.407846;
+    String collectionType = "stores";
+    String entityName = "cornerStore";
+    Double lat = 37.776753;
+    Double lon = -122.407846;
 
-        //1. create an entity with a geo point
-        Map<String, Double> latLon = hashMap("latitude", lat);
-        latLon.put( "longitude", lon );
-        Map<String, Object> entityData = new HashMap<String, Object>();
-        entityData.put( "location", latLon );
-        entityData.put( "name", entityName );
+    //1. create an entity with a geo point
+    Entity entity = new Entity();
+    entity.put("name", entityName);
+    entity.put("location", new MapUtils.HashMapBuilder<String, Double>()
+        .map("latitude", lat)
+        .map("longitude", lon));
 
-        try {
-            entity = context.collection( collectionType ).post( entityData );
-        }
-        catch ( UniformInterfaceException e ) {
-            JsonNode nodeError = mapper.readTree( e.getResponse().getEntity( String.class ) );
-            fail( nodeError.get( "error" ).textValue() );
-        }
-
-        assertNotNull(entity);
-        assertEquals( lat.toString(), entity.get("location").get("latitude").asText() );
-        assertEquals( lon.toString(), entity.get( "location" ).get("longitude").asText() );
-
-        context.refreshIndex();
-
-        //2. read back that entity make sure it is accurate
-        /*
-        try {
-            node = context.collection( collectionType ).get(entityName);
-        }
-        catch ( UniformInterfaceException e ) {
-            JsonNode nodeError = mapper.readTree( e.getResponse().getEntity( String.class ) );
-            fail( nodeError.get( "error" ).textValue() );
-        }
-
-        //3. update the geo point to a new value
-        Double newLat = 35.776753;
-        Double newLon = -119.407846;
-        latLon.put( "latitude", newLat );
-        latLon.put( "longitude", newLon );
-        entityData.put( "location", latLon );
-
-        //TODO PUT should take name property and append it to URL - e.g. PUT /cats/fluffy  not PUT /cats {"name":"fluffy"}
-        try {
-            //node = context.collection( collectionType ).put(entityData);
-            //entity.put(entityData);
-
-        }
-        catch ( UniformInterfaceException e ) {
-            JsonNode nodeError = mapper.readTree( e.getResponse().getEntity( String.class ) );
-            fail( nodeError.get( "error" ).textValue() );
-        }
-
-        assertNotNull(entity);
-        assertEquals( newLat.toString(), entity.get("location").get("latitude").asText() );
-        assertEquals( newLon.toString(), entity.get( "location" ).get("longitude").asText() );
-  */
-
-        context.refreshIndex();
-
-        //4. read back the updated entity, make sure it is accurate
-
-
-
-
-
-    }
-
-    /**
-     * Test exceptions for entities with poorly created geo points
-     * 1. misspell latitude
-     * 2. misspell longitude
-     */
-    @Test
-    public void createEntitiesWithBadSpelling() throws IOException {
-
-        String collectionType = "stores";
-        JsonNode node = null;
-        Double lat = 37.776753;
-        Double lon = -122.407846;
-
-        // 1. misspell latitude
-        Map<String, Double> misspelledLatitude = hashMap("latitudee", lat);
-        misspelledLatitude.put( "longitude", lon );
-        Map<String, Object> misspelledLatitudeEntityData = new HashMap<String, Object>();
-        misspelledLatitudeEntityData.put( "location", misspelledLatitude );
-
-        try {
-            node = context.collection( collectionType ).post( misspelledLatitudeEntityData );
-            fail("System allowed misspelled location property - latitudee, which it should not");
-        }
-        catch ( UniformInterfaceException e ) {
-            //verify the correct error was returned
-            JsonNode nodeError = mapper.readTree( e.getResponse().getEntity( String.class ));
-            assertEquals( "illegal_argument", nodeError.get( "error" ).textValue() );
-        }
-
-        // 2. misspell longitude
-        Map<String, Double> misspelledLongitude = hashMap("latitude", lat);
-        misspelledLongitude.put( "longitudee", lon );
-        Map<String, Object> misspelledLongitudeEntityData = new HashMap<String, Object>();
-        misspelledLongitudeEntityData.put( "location", misspelledLongitude );
-
-        try {
-            node = context.collection( collectionType ).post( misspelledLongitudeEntityData );
-            fail("System allowed misspelled location property - longitudee, which it should not");
-        }
-        catch ( UniformInterfaceException e ) {
-            //verify the correct error was returned
-            JsonNode nodeError = mapper.readTree( e.getResponse().getEntity( String.class ));
-            assertEquals( "illegal_argument", nodeError.get( "error" ).textValue() );
-        }
-
+    Entity testEntity=null;
+    try{
+      testEntity = this.app().collection(collectionType).post(entity);
+    } catch (UniformInterfaceException e) {
+      JsonNode nodeError = mapper.readTree(e.getResponse().getEntity(String.class));
+      fail(nodeError.get("error").textValue());
     }
 
 
-    /**
-     * Test exceptions for entities with poorly created geo points
-     * 1. pass only one point instead of two
-     * 2. pass a values that are not doubles
-     */
-    @Test
-    public void createEntitiesWithBadPoints() throws IOException {
+    assertNotNull(testEntity);
+    assertEquals(lat.toString(), ((HashMap<String, Object>) testEntity.get("location")).get("latitude").toString());
+    assertEquals(lon.toString(), ((HashMap<String, Object>) testEntity.get("location")).get("longitude").toString());
 
-        String collectionType = "stores";
-        JsonNode node = null;
-        Double lat = 37.776753;
-        Double lon = -122.407846;
+//        context.refreshIndex();
 
-        // 1. pass only one point instead of two
-        Map<String, Double> latitudeOnly = hashMap("latitude", lat);
-        Map<String, Object> latitudeOnlyEntityData = new HashMap<String, Object>();
-        latitudeOnlyEntityData.put( "location", latitudeOnly );
+    //2. read back that entity make sure it is accurate
+    try {
+      testEntity = this.app().collection(collectionType).entity(testEntity).get();
+    } catch (UniformInterfaceException e) {
+      JsonNode nodeError = mapper.readTree(e.getResponse().getEntity(String.class));
+      fail(nodeError.get("error").textValue());
+    }
+    assertNotNull(testEntity);
+    assertEquals(lat.toString(), ((HashMap<String, Object>) testEntity.get("location")).get("latitude").toString());
+    assertEquals(lon.toString(), ((HashMap<String, Object>) testEntity.get("location")).get("longitude").toString());
 
-        try {
-            node = context.collection( collectionType ).post( latitudeOnlyEntityData );
-            fail("System allowed location with only one point, latitude, which it should not");
-        }
-        catch ( UniformInterfaceException e ) {
-            //verify the correct error was returned
-            JsonNode nodeError = mapper.readTree( e.getResponse().getEntity( String.class ));
-            assertEquals( "illegal_argument", nodeError.get( "error" ).textValue() );
-        }
+    //3. update the geo point to a new value
+    Double newLat = 35.776753;
+    Double newLon = -119.407846;
+    testEntity.put("location", new MapUtils.HashMapBuilder<String, Double>()
+        .map("latitude", newLat)
+        .map("longitude", newLon));
 
-        // 2. pass a values that are not doubles
-        Map<String, String> notDoubleLatLon = hashMap("latitude", "fred");
-        notDoubleLatLon.put( "longitude", "barney" );
-        Map<String, Object> notDoubleLatLonEntityData = new HashMap<String, Object>();
-        notDoubleLatLonEntityData.put( "location", notDoubleLatLon );
-
-        try {
-            node = context.collection( collectionType ).post( notDoubleLatLonEntityData );
-            fail("System allowed misspelled location values that are not doubles for latitude and longitude, which it should not");
-        }
-        catch ( UniformInterfaceException e ) {
-            //verify the correct error was returned
-            JsonNode nodeError = mapper.readTree( e.getResponse().getEntity( String.class ));
-            assertEquals( "illegal_argument", nodeError.get( "error" ).textValue() );
-        }
-
-
+    try {
+      testEntity = this.app().collection(collectionType).entity(testEntity).put(testEntity);
+    } catch (UniformInterfaceException e) {
+      JsonNode nodeError = mapper.readTree(e.getResponse().getEntity(String.class));
+      fail(nodeError.get("error").textValue());
     }
 
+    assertNotNull(testEntity);
+    assertEquals(newLat.toString(), ((HashMap<String, Object>) testEntity.get("location")).get("latitude").toString());
+    assertEquals(newLon.toString(), ((HashMap<String, Object>) testEntity.get("location")).get("longitude").toString());
+
+    //4. read back the updated entity, make sure it is accurate
+    try {
+      testEntity = this.app().collection(collectionType).entity(testEntity).get();
+    } catch (UniformInterfaceException e) {
+      JsonNode nodeError = mapper.readTree(e.getResponse().getEntity(String.class));
+      fail(nodeError.get("error").textValue());
+    }
+    assertNotNull(testEntity);
+    assertEquals(newLat.toString(), ((HashMap<String, Object>) testEntity.get("location")).get("latitude").toString());
+    assertEquals(newLon.toString(), ((HashMap<String, Object>) testEntity.get("location")).get("longitude").toString());
+
+
+  }
+
+  /**
+   * Test exceptions for entities with poorly created geo points
+   * 1. misspell latitude
+   * 2. misspell longitude
+   */
+  @Test
+  public void createEntitiesWithBadSpelling() throws IOException {
+
+    String collectionType = "stores";
+    JsonNode node = null;
+    Double lat = 37.776753;
+    Double lon = -122.407846;
+
+    // 1. misspell latitude
+    Entity entity = new Entity();
+    entity.put("location", new MapUtils.HashMapBuilder<String, Double>()
+        .map("latitudee", lat) //intentionally misspelled
+        .map("longitude", lon));
+    try {
+      this.app().collection(collectionType).post(entity);
+      fail("System allowed misspelled location property - latitudee, which it should not");
+    } catch (UniformInterfaceException e) {
+      //verify the correct error was returned
+      JsonNode nodeError = mapper.readTree(e.getResponse().getEntity(String.class));
+      assertEquals("illegal_argument", nodeError.get("error").textValue());
+    }
+
+    // 2. misspell longitude
+    entity.put("location", new MapUtils.HashMapBuilder<String, Double>()
+        .map("latitude", lat)
+        .map("longitudee", lon)); //intentionally misspelled
+
+    try {
+      this.app().collection(collectionType).post(entity);
+      fail("System allowed misspelled location property - longitudee, which it should not");
+    } catch (UniformInterfaceException e) {
+      //verify the correct error was returned
+      JsonNode nodeError = mapper.readTree(e.getResponse().getEntity(String.class));
+      assertEquals("illegal_argument", nodeError.get("error").textValue());
+    }
+
+  }
+
+
+  /**
+   * Test exceptions for entities with poorly created geo points
+   * 1. pass only one point instead of two
+   * 2. pass a values that are not doubles
+   */
+  @Test
+  public void createEntitiesWithBadPoints() throws IOException {
+
+    String collectionType = "stores";
+    JsonNode node = null;
+    Double lat = 37.776753;
+    Double lon = -122.407846;
+
+    // 1. pass only one point instead of two
+    Entity entity = new Entity();
+    entity.put("location", new MapUtils.HashMapBuilder<String, Double>()
+        .map("latitude", lat));
+
+    try {
+      this.app().collection(collectionType).post(entity);
+      fail("System allowed location with only one point, latitude, which it should not");
+    } catch (UniformInterfaceException e) {
+      //verify the correct error was returned
+      JsonNode nodeError = mapper.readTree(e.getResponse().getEntity(String.class));
+      assertEquals("illegal_argument", nodeError.get("error").textValue());
+    }
+
+    // 2. pass a values that are not doubles
+    entity.put("location", new MapUtils.HashMapBuilder<String, String>()
+        .map("latitude", "fred")
+        .map("longitude", "barney"));
+
+    try {
+      this.app().collection(collectionType).post(entity);
+      fail("System allowed misspelled location values that are not doubles for latitude and longitude, which it should not");
+    } catch (UniformInterfaceException e) {
+      //verify the correct error was returned
+      JsonNode nodeError = mapper.readTree(e.getResponse().getEntity(String.class));
+      assertEquals("illegal_argument", nodeError.get("error").textValue());
+    }
+  }
 }
