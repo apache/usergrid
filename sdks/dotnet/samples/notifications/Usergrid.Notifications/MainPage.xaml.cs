122607d13d5b551a1474eb4f748948922aa46f2c
Shawn Feldman <sfeldman@apache.org>
2015-01-22 08:47:41 -0700

    push sample

---
+++ sdks/dotnet/samples/notifications/Usergrid.Notifications/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Networking.PushNotifications;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace Usergrid.Notifications
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Usergrid usergrid;
+        private string serverUrl;
+        private string org;
+        private string app;
+        private string notifier;
+        private string apiKey;
+        private string secret;
+        private Guid deviceId;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            var myIp = "10.0.1.20";
+            serverUrl = "http://" + myIp + ":8080";
+            org = "mobile";
+            app = "sandbox";
+            notifier = "winphone";
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+            this.setup();
+        }
+
+        private async void setup()
+        {
+            usergrid = new Usergrid(serverUrl, org, app);
+            await this.usergrid.Authenticate("superuser", "test", true);
+            await usergrid.Push.RegisterDevice(notifier);
+           
+            
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            var message = this.pushText.Text;
+            await usergrid.Push.SendNotification(deviceId,message);
+        }
+
+        private void pushText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private async void AddNotifierButton_Click(object sender, RoutedEventArgs e)
+        {
+            await usergrid.Push.AddNotifier(NotifierName.Text,Sid.Text,Api_Key.Text);
+        }
+    }
+}
---
+++ sdks/dotnet/samples/notifications/Usergrid.Notifications/Usergrid.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Windows.Storage;
+using Windows.Networking.PushNotifications;
+
+namespace Usergrid.Notifications
+{
+    public class Usergrid : UsergridHttpClient
+    {
+        private string url;
+        private string token;
+        private HttpClient client;
+        private PushClient push;
+        private string managementUrl;
+
+        public Usergrid(string url, string org, string app)
+        {
+            string urlWithSlash = url.EndsWith("/",StringComparison.CurrentCulture) ? url : url+"/";
+            this.url = String.Format("{0}{1}/{2}/", urlWithSlash, org, app);
+            this.managementUrl = urlWithSlash + "management/";
+            this.client = new HttpClient();
+            push = new PushClient(this);
+        }
+
+        public async Task Authenticate(string user, string password, bool isManagement)
+        {
+            var url = "token";
+            var jsonObject = new JObject();
+            jsonObject.Add("username", user);
+            jsonObject.Add("password", password);
+            jsonObject.Add("grant_type", "password");
+            HttpResponseMessage response = await SendAsync(HttpMethod.Post,url, jsonObject, isManagement);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                JObject jsonResponse = await GetJsonResponse(response);
+                this.token = jsonResponse.GetValue("access_token").Value<String>();
+                client.DefaultRequestHeaders.Add("X-Authorization", token);
+            }
+            else
+            {
+                throw new Exception("Authentication failed");
+            }
+        }
+
+        public async Task<JObject> GetJsonResponse(HttpResponseMessage response)
+        {
+            JObject jsonResponse = null;
+            using (var stream = await response.Content.ReadAsStreamAsync())
+            {
+                using (var reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    jsonResponse = JObject.Parse(reader.ReadToEnd());
+                }
+            }
+            return jsonResponse;
+        }
+
+        private HttpContent getJsonBody(Object jsonObject)
+        {
+            var content =  new StringContent(JsonConvert.SerializeObject(jsonObject));
+            return content;
+        }
+        public  Task<HttpResponseMessage> SendAsync(HttpMethod method, string url, object obj)
+        {
+            return SendAsync(method, url, obj, false);
+        }
+        public Task<HttpResponseMessage> SendAsync(HttpMethod method, string url, object obj, bool useManagementUrl)
+        {
+            HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Post, (useManagementUrl ? this.managementUrl : this.url) + url);
+            if(obj!=null)
+                message.Content = getJsonBody(obj);
+            message.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue( "application/json"));
+            return this.client.SendAsync(message);
+
+        }
+   
+        public PushClient Push
+        {
+            get { return push; }
+        }
+
+
+    }
+
+    public interface UsergridHttpClient
+    {
+        Task<HttpResponseMessage>SendAsync(HttpMethod method,string url, object obj);
+        Task<JObject> GetJsonResponse(HttpResponseMessage response);
+
+    }
+    public class PushClient
+    {
+        private UsergridHttpClient usergrid;
+        
+        public PushClient(UsergridHttpClient usergrid)
+        { 
+            this.usergrid = usergrid; 
+        }
+
+        public async Task<bool> SendNotification(string message)
+        {
+            if (DeviceId == null)
+            {
+                throw new Exception("Please call PushClient.RegisterDevice first.");
+            }
+            var jsonObject = new JObject();
+            var payloads = new JObject();
+            var payload = new JObject();
+            payload.Add("toast", new JValue(message));
+            payloads.Add("winphone", payload);
+            jsonObject.Add("payloads",payloads);
+            jsonObject.Add("debug", true);
+            var response = await usergrid.SendAsync(HttpMethod.Post,String.Format("devices/{0}/notifications",deviceId), jsonObject);
+            var status = response.StatusCode;
+            JObject jsonResponse = await usergrid.GetJsonResponse(response);
+            return (status == HttpStatusCode.OK);
+        }
+
+        public async Task RegisterDevice(string notifier)
+        {
+            this.Notifier = notifier;
+            var channel = await PushNotificationChannelManager.CreatePushNotificationChannelForApplicationAsync();
+            var settings = ApplicationData.Current.LocalSettings;
+            if (!settings.Values.ContainsKey("currentDeviceId"))
+            {
+                Guid uuid = await registerDevice(notifier, channel.Uri);
+                settings.Values.Add("currentDeviceId", uuid);
+                this.DeviceId = uuid;
+            }
+            else
+            {
+                object tempId;
+                settings.Values.TryGetValue("currentDeviceId", out tempId);
+                this.DeviceId = Guid.Parse(tempId.ToString());
+                if (!await DeviceExists(DeviceId))
+                {
+                    Guid uuid = await registerDevice(notifier, channel.Uri);
+                    settings.Values["currentDeviceId"] = uuid;
+                    this.DeviceId = uuid;
+                }
+
+            }
+
+           
+        }
+      
+        public async Task AddNotifier(string name, string sid, string apiKey)
+        {
+            JObject obj = new JObject();
+            obj.Add("name", new JValue(name));
+            obj.Add("sid", new JValue(sid));
+            obj.Add("provider", new JValue("windows"));
+            obj.Add("apiKey", new JValue(apiKey));
+            obj.Add("logging", new JValue(true));
+            HttpResponseMessage response = await usergrid.SendAsync(HttpMethod.Post, "notifiers", obj);
+
+            JObject jsonResponse = await usergrid.GetJsonResponse(response);
+            if (jsonResponse != null)
+            {
+                var body = jsonResponse.GetValue("entities").ToString();
+            }
+        }
+
+        public async Task<bool> DeviceExists(Guid deviceId)
+        {
+            HttpResponseMessage response = await usergrid.SendAsync(HttpMethod.Get, "devices/"+deviceId, null);
+            JObject jsonResponse = await usergrid.GetJsonResponse(response);
+            if (jsonResponse != null)
+            {
+                    var body = jsonResponse.GetValue("entities");
+                    return body!=null &&  body.Value<JArray>().Count > 0;
+            }
+            else { return false; }
+        }
+
+        private async Task<Guid> registerDevice(string notifier, string uri)
+        {
+            JObject obj = new JObject();
+            obj.Add(notifier + ".notifier.id", new JValue(uri));
+            HttpResponseMessage response = await usergrid.SendAsync(HttpMethod.Post, "devices/", obj);
+
+            JObject jsonResponse = await usergrid.GetJsonResponse(response);
+
+            if (response.StatusCode == HttpStatusCode.OK && jsonResponse != null)
+            {
+                var entity = jsonResponse.GetValue("entities").Value<JArray>()[0];
+                var uuid = Guid.Parse(entity.Value<String>("uuid"));
+                return uuid;
+            }
+            else { return Guid.Empty; }
+        }
+
+        public string Notifier { get; set; }
+        private Guid DeviceId { get; set; }
+
+    }
+}
