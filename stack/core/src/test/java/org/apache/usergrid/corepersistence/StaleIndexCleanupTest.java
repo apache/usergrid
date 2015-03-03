8233916782f4ecffbb2042135a500c369284d4db
Shawn Feldman <sfeldman@apache.org>
2015-03-03 16:47:40 -0700

    add threadsafe back

--- stack/core/src/test/java/org/apache/usergrid/corepersistence/StaleIndexCleanupTest.java
+++ stack/core/src/test/java/org/apache/usergrid/corepersistence/StaleIndexCleanupTest.java
@@ -70,6 +70,7 @@ import static org.junit.Assert.assertTrue;
 /**
  * Test on read style clean-up of stale ElasticSearch indexes.
  */
+@NotThreadSafe
 public class StaleIndexCleanupTest extends AbstractCoreIT {
     private static final Logger logger = LoggerFactory.getLogger( StaleIndexCleanupTest.class );
 
