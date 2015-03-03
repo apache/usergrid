b31088ac1dbd4999ecfcac904372a32765784c3d
Rod Simpson <rod@rodsimpson.com>
2015-03-03 13:16:57 -0700

    Revert "Merge remote-tracking branch 'gerey/USERGRID-280' into apache_usergrid/two-dot-o"
    This reverts commit 67cb71939609d98d2788cccb5b0b33375355d24d, reversing
    changes made to 83b2c236b614e83c8902e8de47177934781d55d5.

--- stack/rest/src/test/java/org/apache/usergrid/rest/test/resource2point0/ClientSetup.java
+++ stack/rest/src/test/java/org/apache/usergrid/rest/test/resource2point0/ClientSetup.java
@@ -41,13 +41,8 @@ import javax.ws.rs.core.MediaType;
 public class ClientSetup implements TestRule {
 
     RestClient restClient;
-
-    protected String username;
-    protected String password;
-    protected String orgName;
-    protected String appName;
-    protected Token superuserToken;
-
+    //TODO: store the password as a string here as well
+    protected String username, orgName, appName;
     protected Organization organization;
     protected Application application;
 
@@ -88,11 +83,7 @@ public class ClientSetup implements TestRule {
         String methodName = description.getMethodName();
         String name = testClass + "." + methodName;
 
-        restClient.superuserSetup();
-        superuserToken = restClient.management().token().post( new Token( "superuser", "superpassword" ) );
-
         username = "user_"+name + UUIDUtils.newTimeUUID();
-        password = username;
         orgName = "org_"+name+UUIDUtils.newTimeUUID();
         appName = "app_"+name+UUIDUtils.newTimeUUID();
 
@@ -106,17 +97,11 @@ public class ClientSetup implements TestRule {
 
     public String getUsername(){return username;}
 
-    public String getPassword(){return password;}
-
     public Organization getOrganization(){return organization;}
 
     public String getOrganizationName(){return orgName;}
 
     public String getAppName() {return appName;}
-
-    public Token getSuperuserToken() {
-        return superuserToken;
-    }
 
     public void refreshIndex() {
         this.restClient.refreshIndex(getOrganizationName(),getAppName());
--- stack/rest/src/test/java/org/apache/usergrid/rest/test/resource2point0/endpoints/mgmt/UsersResource.java
+++ stack/rest/src/test/java/org/apache/usergrid/rest/test/resource2point0/endpoints/mgmt/UsersResource.java
@@ -50,13 +50,9 @@ public class UsersResource extends NamedResource {
         return new EntityEndpoint(identifier, context, this);
     }
 
-    public UserResource user(String identifier) {
-        return new UserResource( identifier, context, this );
-    }
-
     public Entity post(Entity userPayload){
         WebResource resource = getResource(true);
-
+//TODO: need to parse the specific response gotten for admin entities. It is different from regular entities.
         ApiResponse response = resource.type( MediaType.APPLICATION_JSON_TYPE )
                 .accept( MediaType.APPLICATION_JSON ).post( ApiResponse.class, userPayload);
         return new Entity(response);
