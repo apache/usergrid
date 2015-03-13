441483842b14c70bc075437a32cbef52f9460a3c
Shawn Feldman <sfeldman@apache.org>
2015-03-13 11:29:27 -0600

    add replay strategy

--- stack/corepersistence/common/src/test/java/org/apache/usergrid/persistence/core/astyanax/MultiKeyColumnNameIteratorTest.java
+++ stack/corepersistence/common/src/test/java/org/apache/usergrid/persistence/core/astyanax/MultiKeyColumnNameIteratorTest.java
@@ -80,12 +80,12 @@ public class MultiKeyColumnNameIteratorTest {
         final CassandraConfig cassandraConfig = new CassandraConfig() {
             @Override
             public ConsistencyLevel getReadCL() {
-                return ConsistencyLevel.CL_QUORUM;
+                return ConsistencyLevel.CL_LOCAL_ONE;
             }
 
             @Override
             public ConsistencyLevel getConsistentReadCL() {
-                return ConsistencyLevel.CL_LOCAL_ONE;
+                return ConsistencyLevel.CL_LOCAL_QUORUM;
             }
 
 
