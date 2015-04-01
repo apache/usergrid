dd1c268bcfb30a426a86f4994bfbeb253e42ce66
Shawn Feldman <sfeldman@apache.org>
2015-04-01 10:20:18 -0600

    add string parsing to arraylists

--- stack/corepersistence/queryindex/src/main/java/org/apache/usergrid/persistence/index/impl/EntityToMapConverter.java
+++ stack/corepersistence/queryindex/src/main/java/org/apache/usergrid/persistence/index/impl/EntityToMapConverter.java
@@ -70,13 +70,7 @@ public class EntityToMapConverter {
 
             Field field = ( Field ) f;
 
-
-            if ( f instanceof ArrayField) {
-                List list = ( List ) field.getValue();
-                entityMap.put(  field.getName().toLowerCase(),
-                        new ArrayList( processCollectionForMap( list ) ) );
-            }
-            else if ( f instanceof ListField) {
+            if ( f instanceof ListField) {
                 List list = ( List ) field.getValue();
                 entityMap.put(field.getName().toLowerCase(),
                         new ArrayList( processCollectionForMap( list ) ) );
