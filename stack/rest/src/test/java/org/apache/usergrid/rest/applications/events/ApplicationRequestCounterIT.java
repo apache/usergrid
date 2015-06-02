6be835ca8bc4aedf5bd9a82c9c468f7a8f7e99d4
GERey <greyes@apigee.com>
2015-06-02 12:13:51 -0700

    Reworked the counters test to only use the rest tier, not worry about the service AND core tier.

--- stack/rest/src/test/java/org/apache/usergrid/rest/applications/events/ApplicationRequestCounterIT.java
+++ stack/rest/src/test/java/org/apache/usergrid/rest/applications/events/ApplicationRequestCounterIT.java
@@ -17,25 +17,15 @@
 package org.apache.usergrid.rest.applications.events;
 
 
-import java.util.UUID;
-
-import javax.ws.rs.core.MediaType;
-
-import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.apache.usergrid.persistence.EntityManager;
-import org.apache.usergrid.persistence.EntityManagerFactory;
-import org.apache.usergrid.persistence.Query;
-import org.apache.usergrid.persistence.Results;
-import org.apache.usergrid.persistence.index.query.CounterResolution;
-import org.apache.usergrid.rest.AbstractRestIT;
-import org.apache.usergrid.services.ServiceManager;
-import org.apache.usergrid.utils.UUIDUtils;
+import org.apache.usergrid.rest.test.resource2point0.AbstractRestIT;
+import org.apache.usergrid.rest.test.resource2point0.model.ApiResponse;
+import org.apache.usergrid.rest.test.resource2point0.model.Collection;
+import org.apache.usergrid.rest.test.resource2point0.model.QueryParameters;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 
@@ -49,49 +39,19 @@ public class ApplicationRequestCounterIT extends AbstractRestIT {
     private static final Logger log = LoggerFactory.getLogger( ApplicationRequestCounterIT.class );
     long ts = System.currentTimeMillis() - ( 24 * 60 * 60 * 1000 );
 
-
     @Test
     public void applicationrequestInternalCounters() throws Exception {
-        // Get application id
-        JsonNode node = mapper.readTree( resource().path( "/test-organization/test-app" ).queryParam( "access_token", access_token )
-                .accept( MediaType.APPLICATION_JSON ).type( MediaType.APPLICATION_JSON_TYPE ).get( String.class ));
 
-        assertNotNull( node.get( "entities" ) );
+        //get default application
+        ApiResponse defaultApp = org().app( clientSetup.getAppName() ).get();
 
-        String uuid = node.get( "application" ).asText();
-        assertEquals( true, UUIDUtils.isUUID( uuid ) );
+        QueryParameters queryParameters = new QueryParameters();
+        queryParameters.addParam( "resolution", "all" ).addParam( "counter","application.requests" );
+        Collection countersResponse = org().app( clientSetup.getAppName() ).collection( "counters" ).get( queryParameters ,true );
 
-        refreshIndex("test-organization", "test-app");
+        assertNotNull( countersResponse );
+//        ArrayList counterValues = ( ArrayList ) countersResponse.getResponse().getProperties().get( "counters" );
+//        assertEquals( 1, counterValues.indexOf( 1 ));
 
-        UUID applicationId = UUID.fromString( uuid );
-        EntityManagerFactory emf = setup.getEmf();
-        EntityManager em = emf.getEntityManager( applicationId );
-
-        int beforeTotalCall = getCounter( em, ServiceManager.APPLICATION_REQUESTS );
-        int beforeCall = getCounter( em, ServiceManager.APPLICATION_REQUESTS_PER.concat( "get" ) );
-
-        // call
-        node = mapper.readTree( resource().path( "/test-organization/test-app/counters" ).queryParam( "resolution", "all" )
-                .queryParam( "counter", "application.requests" ).queryParam( "access_token", adminToken() )
-                .accept( MediaType.APPLICATION_JSON ).type( MediaType.APPLICATION_JSON_TYPE ).get( String.class ));
-
-        assertNotNull( node.get( "counters" ) );
-
-        int afterTotalCall = getCounter( em, ServiceManager.APPLICATION_REQUESTS );
-        int afterCall = getCounter( em, ServiceManager.APPLICATION_REQUESTS_PER.concat( "get" ) );
-
-        assertEquals( 1, afterCall - beforeCall );
-        assertEquals( 1, afterTotalCall - beforeTotalCall );
-    }
-
-
-    private int getCounter( EntityManager em, String key ) throws Exception {
-        Query query = new Query();
-        query.addCounterFilter( key + ":*:*:*" );
-        query.setStartTime( ts );
-        query.setFinishTime( System.currentTimeMillis() );
-        query.setResolution( CounterResolution.ALL );
-        Results r = em.getAggregateCounters( query );
-        return ( int ) r.getCounters().get( 0 ).getValues().get( 0 ).getValue();
     }
 }
