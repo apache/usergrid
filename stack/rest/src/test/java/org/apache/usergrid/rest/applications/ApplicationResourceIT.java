97647019d9d0086578065beaee8082ae44f9a105
Shawn Feldman <sfeldman@apache.org>
2015-06-11 11:34:39 -0600

    unignore

--- stack/rest/src/test/java/org/apache/usergrid/rest/applications/ApplicationResourceIT.java
+++ stack/rest/src/test/java/org/apache/usergrid/rest/applications/ApplicationResourceIT.java
@@ -427,7 +427,6 @@ public class ApplicationResourceIT extends AbstractRestIT {
      * Update the default auth token TTL for an application
      */
     @Test
-    @Ignore
     public void updateAccessTokenTtl() throws Exception {
 
         String orgName = clientSetup.getOrganizationName().toLowerCase();
