18ca24ca5eba8d82b827a864d5fadc5164618a78
Shawn Feldman <sfeldman@apache.org>
2015-06-17 09:53:55 -0600

    fix index notifications push errors

--- stack/services/src/main/java/org/apache/usergrid/services/notifications/QueueListener.java
+++ stack/services/src/main/java/org/apache/usergrid/services/notifications/QueueListener.java
@@ -237,7 +237,7 @@ public class QueueListener  {
                             }
                         }
                     })
-                    .toBlocking().last();
+                    .toBlocking().lastOrDefault(null);
 
 
         }
