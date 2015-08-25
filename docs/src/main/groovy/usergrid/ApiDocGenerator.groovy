25bd190d5ca420251f70becb1b64d5591b4a80d4
Dave Johnson <snoopdave@apache.org>
2015-08-25 10:41:28 -0400

    Adding cross-references to Swagger generated docs.

--- docs/src/main/groovy/usergrid/ApiDocGenerator.groovy
+++ docs/src/main/groovy/usergrid/ApiDocGenerator.groovy
@@ -5,22 +5,31 @@
 // Depdency management with Grape:
 //    http://docs.groovy-lang.org/latest/html/documentation/grape.html
 
-package usergrid;
+package usergrid
 
 @Grab(group = 'io.swagger', module = 'swagger-parser', version = '1.0.8')
 @Grab(group = 'io.swagger', module = 'swagger-compat-spec-parser', version = '1.0.8')
 @Grab(group = 'com.github.spullara.mustache.java', module = 'compiler', version = '0.8.18-SNAPSHOT')
+@Grab(group = 'org.slf4j', module = 'slf4j-simple', version = '1.7.12')
 
-import io.swagger.parser.*;
-import io.swagger.models.*;
-import com.github.mustachejava.*
-import org.apache.commons.lang3.RandomStringUtils;
+import com.github.mustachejava.DefaultMustacheFactory
+import io.swagger.models.Model
+import io.swagger.models.Operation
+import io.swagger.models.parameters.BodyParameter
+import io.swagger.models.parameters.RefParameter
+import io.swagger.models.properties.RefProperty
+import io.swagger.parser.*
+import org.apache.commons.lang3.RandomStringUtils
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 
 /**
  * Generates Usergrid API docs from Swagger in Markdown format.
  */
 public class ApiDocGenerator {
+   
+    static Logger logger = LoggerFactory.getLogger(ApiDocGenerator.class);
     
     def parser = new SwaggerParser();
     def swagger = parser.read("src/main/resources/usergrid-swagger.yaml");
@@ -39,8 +48,14 @@ public class ApiDocGenerator {
     
     def allTags = [];
     def allModels = [];
-
-
+    def urlOpsBySchema = [:];
+    def modelsBySchema = [:];
+    
+    def tagsToUrlOps = new TreeMap();
+    
+    def definitions = new TreeMap();
+    def subTypes = new TreeMap();
+    
     public ApiDocGenerator() {
         writer = new OutputStreamWriter(new FileOutputStream("rest-endpoints/api-docs.md"));
         operationTemplate = mf.compile(
@@ -61,59 +76,66 @@ public class ApiDocGenerator {
     }
     
     def generate() {
-       
-        // build up scope and generate via Mustache template
-        
-        // organize methods by tag
-        def tagsToUrlOps = new TreeMap();
+      
+        // build various hashmaps and lists to help organize and cross-reference content
         
         swagger.getPaths().each { pathEntry ->
             def url = pathEntry.key;
             def path = pathEntry.value;
            
             if (path.get != null) {
-                addOperation(tagsToUrlOps, "GET", url, path.get);
+                addOperation("GET", url, path.get);
             }
             if (path.post != null) {
-                addOperation(tagsToUrlOps, "POST", url, path.post);
+                addOperation("POST", url, path.post);
             }
             if (path.put != null) {
-                addOperation(tagsToUrlOps, "PUT", url, path.put);
+                addOperation("PUT", url, path.put);
             }
             if (path.delete != null) {
-                addOperation(tagsToUrlOps, "DELETE", url, path.delete);
+                addOperation("DELETE", url, path.delete);
             }
         };
-
         tagsToUrlOps.each { entry -> allTags.add(entry.key) };
 
-        def definitions = new TreeMap();
         swagger.getDefinitions().each { entry ->
-            definitions.put( entry.key, entry.value );
+            addModel( entry.key, entry.value );
         };
         definitions.each { entry -> allModels.add(entry.key); };
-       
-        // generate
+        
+        // generate the doc
         
         generateFileStart();
+        
         generateMethodsSectionTitle();
+        
         tagsToUrlOps.each { entry -> 
             def tag = entry.key;
             def urlOps = entry.value;
             generateMethodsTitle(tag);
             urlOps.each { urlOp -> formatOperation( urlOp ); };
         };
-        
+
         generateModelsTitle();
+        
         definitions.each { entry ->
             def name = entry.key;
             def model = entry.value;
             formatModel( name, model );
         };
+        
+        generateSubTypesTitle();
+        
+        subTypes.each { entry ->
+            def name = entry.key;
+            def model = entry.value;
+            formatModel( name, model );
+        };
+        
         generateFileEnd();
     }
     
-    def addOperation( Map tagsToUrlOps, String method, String url, Operation operation ) {
+    def addOperation( String method, String url, Operation operation ) {
        
         // assume each operation has one tag
         def tag = operation.tags[0];
@@ -123,12 +145,67 @@ public class ApiDocGenerator {
             urlOps = [];
             tagsToUrlOps[tag] = urlOps;
         }
-
+        
         def urlOp = new HashMap();
         urlOp.url = url;
         urlOp.method = method;
         urlOp.operation = operation;
+        urlOp.opId = RandomStringUtils.randomAlphanumeric(10);
         urlOps.add(urlOp);
+
+        operation.getResponses().each { responseEntry ->
+
+            if (responseEntry.value.schema != null) {
+
+                if (responseEntry.key.equals("200")) {
+                    def list = urlOpsBySchema[responseEntry.value.schema.ref];
+                    if (list == null) {
+                        list = new HashSet();
+                        urlOpsBySchema[responseEntry.value.schema.ref] = list;
+                    }
+                    list.add(urlOp);
+                }
+            }
+        };
+
+        operation.getParameters().each { param ->
+            
+            if ( param instanceof BodyParameter && param.schema != null ) {
+                def list = urlOpsBySchema[param.schema.ref];
+                if (list == null) {
+                    list = new HashSet();
+                    urlOpsBySchema[param.schema.ref] = list;
+                }
+                list.add(urlOp);
+            }
+        };
+        
+    }
+    
+    def addModel(name, model) {
+       
+        if (urlOpsBySchema[name] != null || name.equals("Error")) {
+            definitions.put(name, model);
+        } else if (modelsBySchema[name] != null ) {
+            subTypes.put(name, model);
+        } else {
+            logger.error("${name} omitted because it is not referenced by any API path or definition.");
+        }
+        
+        model.properties.each { propertyEntity ->
+            
+            if (propertyEntity.value instanceof RefProperty ) {
+                def list = modelsBySchema[ propertyEntity.value.ref ];
+                if ( list == null ) {
+                    list = [];
+                    modelsBySchema[ propertyEntity.value.ref ] = list;
+                }
+                def modelNames = [:];
+                modelNames.refName = name;
+                modelNames.refName_lc = name.toLowerCase();
+                list.add(modelNames);
+            }
+        };
     }
 
     def formatOperation( urlOp ) {
@@ -150,7 +227,17 @@ public class ApiDocGenerator {
             // if parameter has a schema, assume that it is a reference
             if ( responseEntry.value.schema != null) {
                 response.schema = responseEntry.value.schema.ref;
-                response.schemaAnchor = responseEntry.value.schema.ref.toLowerCase();
+                response.schemaAnchor = responseEntry.value.schema.ref;
+               
+                // keep track of paths that use each schema definition
+                if ( response.status.equals("200") ) {
+                    def list = urlOpsBySchema[response.schema];
+                    if ( list == null ) {
+                        list = [];
+                        urlOpsBySchema[response.schema] = list;
+                    }
+                    list.add(urlOp);
+                }
             }
             responses.add(response);
         }
@@ -159,15 +246,26 @@ public class ApiDocGenerator {
         op.getParameters().each { parameter -> 
             def param = [:];
             param.name = parameter.name;
+            param.name_lc = parameter.name.toLowerCase();
             param.required = parameter.required;
             param.description = parameter.description;
             param.in = parameter.in;
             
             // assume that body parameters have a schema that is a reference
-            if (parameter.in == "body" && parameter.schema != null) {
+            if (parameter.in == "body") {
+                
                 param.schemaRef = parameter.schema.ref;
                 param.schemaAnchor = parameter.schema.ref.toLowerCase();
-            } else { // if (parameter.in == "path") {
+
+                // keep track of paths that use each schema definition
+                def list = urlOpsBySchema[parameter.schema.ref];
+                if ( list == null ) {
+                    list = [];
+                    urlOpsBySchema[parameter.schema.ref] = list;
+                }
+                list.add(urlOp);
+
+            } else if ( !(parameter instanceof RefParameter) ) {
                 param.type = parameter.type;
             }
             params.add(param);
@@ -181,14 +279,21 @@ public class ApiDocGenerator {
         scope.tags = op.getTags();
         scope.responses = responses;
         scope.parameters = params;
-        scope.opid = RandomStringUtils.randomAlphanumeric(10);
-       
+        scope.opId = urlOp.opId;
+        
         operationTemplate.execute(writer, scope);
         writer.flush();
     }
     
     def formatModel(String name, Model model) {
 
+        def scope = [:];
+        scope.referrers = urlOpsBySchema[name];
+        scope.modelRefs = modelsBySchema[name];
+        scope.hasReferrers = scope.referrers != null && !scope.referrers.isEmpty();
+        scope.hasParamRefs = scope.paramRefs != null && !scope.paramRefs.isEmpty();
+        scope.hasModelRefs = scope.modelRefs != null && !scope.modelRefs.isEmpty();
+        
         // put properties in array form, mustache doesn't play nice with associative arrays
         def props = [];
         model.getProperties().each { property -> 
@@ -201,12 +306,17 @@ public class ApiDocGenerator {
             prop.readOnly = property.value.readOnly;
             prop.required = property.value.required;
             prop.position = property.value.position;
+            if ( property.value instanceof RefProperty ) {
+                prop.ref = property.value.ref;
+                prop.ref_lc = property.value.ref.toLowerCase();
+            }
             props.add(prop);   
         };
-        def scope = [:];
         scope.name = name;
+        scope.name_lc = name.toLowerCase();
+        scope.description = model.description;
         scope.properties = props;
-        scope.modelid = RandomStringUtils.randomAlphanumeric(10);
+       
         modelTemplate.execute(writer, scope);
         writer.flush(); 
     }
@@ -217,7 +327,7 @@ public class ApiDocGenerator {
         allTags.each{ tag -> 
             def atag = [:];
             atag.name = tag;
-            atag.link = tag.toLowerCase();
+            atag.link = tag;
             tags.add(atag);
         };
         scope.tags = tags;
@@ -239,7 +349,12 @@ public class ApiDocGenerator {
     
     def generateModelsTitle() {
         writer.println "\n## Models";
-        writer.println "Properties for Usergrid default entities.";
+        writer.println "This section lists the properties for the Usergrid Default Entities:";
+    }
+
+    def generateSubTypesTitle() {
+        writer.println "\n## Sub-Types";
+        writer.println "This section lists the properties for sub-types used in Usergrid Default Entities.";
     }
     
     def generateFileEnd() {
@@ -289,9 +404,15 @@ class HtmlApiDocGenerator extends ApiDocGenerator {
     def generateModelsTitle() {
         writer.println "<a name='models'></a>";
         writer.println "<br><h1>Default Entity Models</h1>" +
-                "<p>This section lists the properties for the following Usergrid Default Entities:</p>";
+                "<p>This section lists the properties for the following Usergrid Default Entities.</p>";
     }
 
+    def generateSubTypesTitle() {
+        writer.println "<a name='subtypes'></a>";
+        writer.println "<br><h1>Sub-types Referenced By Models</h1>" +
+                "<p>This section lists the properties for sub-types used in Usergrid Default Entities.</p>";
+    }
+    
     def generateFileEnd() {
         def scope = [:];
         fileEndTemplate.execute(writer, scope);
