1c742af77bc9edec078580aed700c2ef60a3def5
Shawn Feldman <sfeldman@apache.org>
2015-09-08 16:23:07 -0600

    adding status service

--- stack/core/src/main/java/org/apache/usergrid/corepersistence/service/StatusServiceImpl.java
+++ stack/core/src/main/java/org/apache/usergrid/corepersistence/service/StatusServiceImpl.java
@@ -19,9 +19,16 @@
  */
 package org.apache.usergrid.corepersistence.service;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
+import org.apache.usergrid.corepersistence.util.CpNamingUtils;
+import org.apache.usergrid.persistence.core.util.StringUtils;
 import org.apache.usergrid.persistence.map.MapManager;
 import org.apache.usergrid.persistence.map.MapManagerFactory;
+import org.apache.usergrid.persistence.map.impl.MapScopeImpl;
+import org.apache.usergrid.persistence.model.entity.Id;
+import org.apache.usergrid.persistence.model.entity.SimpleId;
 import rx.Observable;
 
 import java.util.Map;
@@ -33,6 +40,10 @@ import java.util.UUID;
 public class StatusServiceImpl implements StatusService {
 
     private final MapManagerFactory mapManagerFactory;
+    final static String statusKey = "status";
+    final static String dataKey = "data";
+    private final JsonFactory JSON_FACTORY = new JsonFactory();
+    private final ObjectMapper MAPPER = new ObjectMapper( JSON_FACTORY );
 
     @Inject
     public StatusServiceImpl(final MapManagerFactory mapManagerFactory){
@@ -40,13 +51,38 @@ public class StatusServiceImpl implements StatusService {
     }
 
     @Override
-    public Observable<UUID> setStatus( UUID jobStatusId, Status status, Map<String, Object> data) {
-//        mapManager.putUuid(jobStatusId+"ke",);
-        return null;
+    public Observable<UUID> setStatus(final UUID applicationId, final UUID jobId, final Status status, final Map<String, Object> data) {
+        return Observable.create(sub -> {
+            final String jobString = StringUtils.sanitizeUUID(jobId);
+            final Id appId = CpNamingUtils.generateApplicationId(applicationId);
+            final MapManager mapManager = mapManagerFactory.createMapManager(new MapScopeImpl(appId, "status"));
+            try {
+                final String dataString = MAPPER.writeValueAsString(data);
+                mapManager.putString(jobString + dataKey, dataString);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            mapManager.putString(jobString + statusKey, status.toString());
+            sub.onNext(jobId);
+            sub.onCompleted();
+        });
     }
 
     @Override
-    public Observable<JobStatus> getStatus(UUID jobId) {
-        return null;
+    public Observable<JobStatus> getStatus(final UUID applicationId, UUID jobId) {
+        final String jobString = StringUtils.sanitizeUUID(jobId);
+
+        return Observable.create(subscriber -> {
+            Id appId = CpNamingUtils.generateApplicationId(applicationId);
+            final MapManager mapManager = mapManagerFactory.createMapManager(new MapScopeImpl(appId, "status"));
+            try {
+                final Map<String, Object> data = MAPPER.readValue(mapManager.getString(jobString+dataKey), Map.class);
+                final Status status = Status.valueOf(mapManager.getString(jobString+statusKey));
+                subscriber.onNext(new JobStatus(jobId,status,data));
+                subscriber.onCompleted();
+            }catch (Exception e){
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
