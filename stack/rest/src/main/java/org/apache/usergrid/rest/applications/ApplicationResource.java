1b26c665886526cd43e715fb1e92a338e3e6cbcb
Michael Russo <michaelarusso@gmail.com>
2015-12-04 09:59:49 -0800

    Removed unused import.

--- stack/rest/src/main/java/org/apache/usergrid/rest/applications/ApplicationResource.java
+++ stack/rest/src/main/java/org/apache/usergrid/rest/applications/ApplicationResource.java
@@ -38,7 +38,6 @@ import javax.ws.rs.core.PathSegment;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
-import org.apache.usergrid.rest.security.annotations.CheckPermissionsForPath;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
