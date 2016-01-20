78bc3d5c06c25e2a2e7280e9d87e87f169b328b7
George Reyes <grey@apache.org>
2016-01-20 15:02:20 -0800

    ElasticsearchException added so we return 503's if we don't do GETs.

---
+++ stack/rest/src/main/java/org/apache/usergrid/rest/exceptions/ElasticsearchExecutionExceptionMapper.java
@@ -0,0 +1,24 @@
+package org.apache.usergrid.rest.exceptions;
+
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
+import org.elasticsearch.action.search.SearchPhaseExecutionException;
+
+import static javax.ws.rs.core.Response.Status.SERVICE_UNAVAILABLE;
+
+
+/**
+ * Created by ApigeeCorporation on 1/19/16.
+ */
+@Provider
+public class ElasticsearchExecutionExceptionMapper
+    extends AbstractExceptionMapper<SearchPhaseExecutionException>  {
+
+    @Override
+    public Response toResponse( SearchPhaseExecutionException spee ){
+        return toResponse( SERVICE_UNAVAILABLE, spee );
+
+    }
+}
