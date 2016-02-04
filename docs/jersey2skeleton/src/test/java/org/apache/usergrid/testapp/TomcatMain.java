5ed3d753107125d24a7666225d176ed4464d3fe0
Dave Johnson <snoopdave@apache.org>
2016-02-04 10:48:20 -0800

    Bring in docs and website from master.

---
+++ docs/jersey2skeleton/src/test/java/org/apache/usergrid/testapp/TomcatMain.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2014 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.usergrid.testapp;
+
+import com.google.common.io.Files;
+import org.apache.catalina.startup.Tomcat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
+
+/**
+ * Simple wrapper for starting "embedded" Tomcat as it's own process, for testing.
+ */
+public class TomcatMain {
+    
+    private static final Logger log = LoggerFactory.getLogger( TomcatMain.class );
+
+    public static void main(String[] args) throws Exception {
+
+        String webappsPath = args[0];
+        int port = Integer.parseInt( args[1] );
+
+        File dataDir = Files.createTempDir();
+        dataDir.deleteOnExit();
+
+        Tomcat tomcat = new Tomcat();
+        tomcat.setBaseDir(dataDir.getAbsolutePath());
+        tomcat.setPort(port);
+        tomcat.getConnector().setAttribute("maxThreads", "1000");
+        tomcat.addWebapp("/", new File(webappsPath).getAbsolutePath());
+
+        log.info("-----------------------------------------------------------------");
+        log.info("Starting Tomcat port {} dir {}", port, webappsPath);
+        log.info("-----------------------------------------------------------------");
+        tomcat.start();
+
+        while ( true ) {
+            Thread.sleep(1000);
+        }
+    }
+
+}
