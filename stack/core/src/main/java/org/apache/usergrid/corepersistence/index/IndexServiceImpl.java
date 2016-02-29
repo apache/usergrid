edd009dd351a4b939beb96486b598edac7e1c0ed
Michael Russo <mrusso@apigee.com>
2016-02-28 22:03:36 -0800

    Fix buffer to use the proper interface of timespan, unit, count

--- stack/core/src/main/java/org/apache/usergrid/corepersistence/index/IndexServiceImpl.java
+++ stack/core/src/main/java/org/apache/usergrid/corepersistence/index/IndexServiceImpl.java
@@ -106,7 +106,7 @@ public class IndexServiceImpl implements IndexService {
         //do our observable for batching
         //try to send a whole batch if we can
         final Observable<IndexOperationMessage>  batches =  sourceEdgesToIndex
-            .buffer( indexFig.getIndexBatchSize(), 1000, TimeUnit.MILLISECONDS )
+            .buffer(250, TimeUnit.MILLISECONDS, indexFig.getIndexBatchSize() )
 
             //map into batches based on our buffer size
             .flatMap( buffer -> Observable.from( buffer )
