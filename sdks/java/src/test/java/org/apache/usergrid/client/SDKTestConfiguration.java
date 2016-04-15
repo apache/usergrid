2bcafe6a6e24bf44061f2454262e25b7c330ac32
Robert Walsh <rjwalsh1985@gmail.com>
2016-04-15 08:25:39 -0500

    Updating test configuration constants.

--- sdks/java/src/test/java/org/apache/usergrid/client/SDKTestConfiguration.java
+++ sdks/java/src/test/java/org/apache/usergrid/client/SDKTestConfiguration.java
@@ -22,11 +22,11 @@ public class SDKTestConfiguration {
     public static final String APP_CLIENT_ID = "b3U6THNcevskEeOQZLcUROUUVA"; //"YXA6_j0WsfFCEeWKoy6txsCOfA" ;
     public static final String APP_CLIENT_SECRET = "b3U6RZHYznP28xieBzQPackFPmmnevU"; //"YXA6jg8x4wjq1AAyQBKtn4bRd1l0gJ8"; //
 
-    public static final String APP_UserName = "rwalsh"; //"test";// //"b3U66ne33W4OEeWXmAIj6QFb-Q";
+    public static final String APP_UserName = "javaSDK"; //"test";// //"b3U66ne33W4OEeWXmAIj6QFb-Q";
     public static final String APP_Password = "Apigee123"; //"test";//"b3U6PxbpQiTrXKCWu0n1CjK1uTZXuG4";
     public static final String USERGRID_URL = "https://api.usergrid.com/";
     public static final String ORG_NAME = "rwalsh";
-    public static final String APP_NAME = "sandbox";
+    public static final String APP_NAME = "sdk.demo";
 
     public static UsergridAuthMode authFallBack = UsergridAuthMode.APP;
 
