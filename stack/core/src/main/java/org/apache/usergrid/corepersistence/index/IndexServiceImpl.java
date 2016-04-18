12f32eb3a12e5b37d8de342502bc62320e79c4e4
Dave Johnson <snoopdave@apache.org>
2016-04-18 08:26:37 -0400

    Do not add "none" to list of required fields!

--- stack/core/src/main/java/org/apache/usergrid/corepersistence/index/IndexServiceImpl.java
+++ stack/core/src/main/java/org/apache/usergrid/corepersistence/index/IndexServiceImpl.java
@@ -226,6 +226,9 @@ public class IndexServiceImpl implements IndexService {
                 return Optional.absent();
             }
 
+
+            // never add "none" because it has special meaning, "none" disables indexing for a type
+            //Do not add "none" to list of required fields!
             fieldsToKeep.remove("none");
 
             defaultProperties.addAll( fieldsToKeep );
