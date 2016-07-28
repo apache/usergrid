3b9e3c4645473c12e4627bea181d9e2c329fda18
Jeff West <jwest@apigee.com>
2016-07-28 14:09:34 -0700

    updated Apache license header

---
+++ sdks/python/README.rst
@@ -0,0 +1,20 @@
+**********
+Overview
+**********
+
+This is a starter project for the Usergrid Python SDK.  It is a work in progress.
+
+**************************
+Installation
+**************************
+
+================================================
+Installation From Pypi Using PIP
+================================================
+
+PIP is a package manager for Python.  For more information please view the information here: `PIP Installation Guide <http://pip.readthedocs.org/en/stable/installing/>`_
+
+From the command line::
+
+  pip install usergrid
+
--- sdks/python/sample_app.py
+++ sdks/python/sample_app.py
@@ -1,23 +1,25 @@
-#
-#  Licensed to the Apache Software Foundation (ASF) under one or more
-#   contributor license agreements.  The ASF licenses this file to You
-#  under the Apache License, Version 2.0 (the "License"); you may not
-#  use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.  For additional information regarding
-#  copyright in this work, please see the NOTICE file in the top level
-#  directory of this distribution.
+# */
+# * Licensed to the Apache Software Foundation (ASF) under one
+# * or more contributor license agreements.  See the NOTICE file
+# * distributed with this work for additional information
+# * regarding copyright ownership.  The ASF licenses this file
+# * to you under the Apache License, Version 2.0 (the
+# * "License"); you may not use this file except in compliance
+# * with the License.  You may obtain a copy of the License at
+# *
+# *   http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing,
+# * software distributed under the License is distributed on an
+# * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# * KIND, either express or implied.  See the License for the
+#    * specific language governing permissions and limitations
+# * under the License.
+# */
 
 from usergrid import Usergrid
 
-__author__ = 'ApigeeCorporation'
+__author__ = 'Jeff West @ ApigeeCorporation'
 
 
 def main():
@@ -25,11 +27,13 @@ def main():
                   app_id='sandbox')
 
     response = Usergrid.DELETE('pets', 'max')
+
     if not response.ok:
         print 'Failed to delete max: %s' % response
         exit()
 
     response = Usergrid.DELETE('owners', 'jeff')
+
     if not response.ok:
         print 'Failed to delete Jeff: %s' % response
         exit()
@@ -38,12 +42,16 @@ def main():
 
     if response.ok:
         pet = response.first()
+
         print pet
+
         response = Usergrid.POST('owners', {'name': 'jeff'})
 
         if response.ok:
             owner = response.first()
+
             print owner
+
             response = pet.connect('ownedBy', owner)
 
             if response.ok:
@@ -55,6 +63,7 @@ def main():
                     print 'all done!'
                 else:
                     print response
+
             else:
                 print 'failed to connect: %s' % response
 
