apache/usergrid

commit aa44b067aacc04bab9aea6799ae23604466cea21

Initial commit for exposing collection re-index to non sysadmin users.

stack/core/src/main/java/org/apache/usergrid/corepersistence/index/ReIndexService.java
@@ -20,6 +20,8 @@
 package org.apache.usergrid.corepersistence.index;
 
 
+import org.apache.usergrid.utils.StringUtils;
+
 /**
  * An interface for re-indexing all entities in an application
  */
@@ -47,6 +49,13 @@ public interface ReIndexService {
      */
     ReIndexStatus getStatus( final String jobId );
 
+    /**
+     * Get the status of a collection job
+     * @param collectionName The collectionName for the rebuild index
+     * @return
+     */
+    ReIndexStatus getStatusForCollection( final String appIdString, final String collectionName );
+
 
     /**
      * The response when requesting a re-index operation
@@ -56,14 +65,27 @@ public interface ReIndexService {
         final Status status;
         final long numberProcessed;
         final long lastUpdated;
+        final String collectionName;
 
 
         public ReIndexStatus( final String jobId, final Status status, final long numberProcessed,
-                              final long lastUpdated ) {
-            this.jobId = jobId;
+                              final long lastUpdated, final String collectionName ) {
+
+            if(StringUtils.isNotEmpty(jobId)){
+                this.jobId = jobId;
+            }else {
+                this.jobId = "";
+            }
+
             this.status = status;
             this.numberProcessed = numberProcessed;
             this.lastUpdated = lastUpdated;
+
+            if(StringUtils.isNotEmpty(collectionName)){
+                this.collectionName = collectionName;
+            }else {
+                this.collectionName = "";
+            }
         }
 
 
@@ -72,6 +94,13 @@ public interface ReIndexService {
          */
         public String getJobId() {
             return jobId;
+        }
+
+        /**
+         * Get the jobId used to resume this operation
+         */
+        public String getCollectionName() {
+            return collectionName;
         }
 
 

stack/core/src/main/java/org/apache/usergrid/corepersistence/index/ReIndexServiceImpl.java
@@ -75,6 +75,7 @@ public class ReIndexServiceImpl implements ReIndexService {
     private static final String MAP_COUNT_KEY = "count";
     private static final String MAP_STATUS_KEY = "status";
     private static final String MAP_UPDATED_KEY = "lastUpdated";
+    private static final String MAP_SEPARATOR = "|||";
 
 
     private final AllApplicationsObservable allApplicationsObservable;
@@ -140,7 +141,9 @@ public class ReIndexServiceImpl implements ReIndexService {
 
         // create an observable that loads a batch to be indexed
 
-        if(reIndexRequestBuilder.getCollectionName().isPresent()) {
+        final boolean isForCollection = reIndexRequestBuilder.getCollectionName().isPresent();
+
+        if(isForCollection) {
 
             String collectionName =  InflectionUtils.pluralize(
                 CpNamingUtils.getNameFromEdgeType(reIndexRequestBuilder.getCollectionName().get() ));
@@ -175,12 +178,36 @@ public class ReIndexServiceImpl implements ReIndexService {
                 if( edgeScopes.size() > 0 ) {
                     writeCursorState(jobId, edgeScopes.get(edgeScopes.size() - 1));
                 }
-                writeStateMeta( jobId, Status.INPROGRESS, count.get(), System.currentTimeMillis() ); })
-            .doOnCompleted(() -> writeStateMeta( jobId, Status.COMPLETE, count.get(), System.currentTimeMillis() ))
+                if( isForCollection ){
+                    writeStateMetaForCollection(
+                        appId.get().getApplication().getUuid().toString(),
+                        reIndexRequestBuilder.getCollectionName().get(),
+                        Status.INPROGRESS, count.get(),
+                        System.currentTimeMillis() );
+                }else{
+                    writeStateMeta( jobId, Status.INPROGRESS, count.get(), System.currentTimeMillis() );
+                }
+            })
+            .doOnCompleted(() ->{
+                if( isForCollection ){
+                    writeStateMetaForCollection(
+                        appId.get().getApplication().getUuid().toString(),
+                        reIndexRequestBuilder.getCollectionName().get(),
+                        Status.COMPLETE, count.get(),
+                        System.currentTimeMillis() );
+                }else {
+                    writeStateMeta(jobId, Status.COMPLETE, count.get(), System.currentTimeMillis());
+                }
+            })
             .subscribeOn( Schedulers.io() ).subscribe();
 
+        if(isForCollection){
+            return new ReIndexStatus( "", Status.STARTED, 0, 0, reIndexRequestBuilder.getCollectionName().get() );
 
-        return new ReIndexStatus( jobId, Status.STARTED, 0, 0 );
+        }
+
+
+        return new ReIndexStatus( jobId, Status.STARTED, 0, 0, "" );
     }
 
 
@@ -196,36 +223,13 @@ public class ReIndexServiceImpl implements ReIndexService {
         return getIndexResponse( jobId );
     }
 
-
-    /**
-     * Simple collector that counts state, then flushed every time a buffer is provided.  Writes final state when complete
-     */
-    private class FlushingCollector {
-
-        private final String jobId;
-        private long count;
-
-
-        private FlushingCollector( final String jobId ) {
-            this.jobId = jobId;
-        }
-
-
-        public void flushBuffer( final List<EdgeScope> buffer ) {
-            count += buffer.size();
-
-            //write our cursor state
-            if ( buffer.size() > 0 ) {
-                writeCursorState( jobId, buffer.get( buffer.size() - 1 ) );
-            }
-
-            writeStateMeta( jobId, Status.INPROGRESS, count, System.currentTimeMillis() );
-        }
-
-        public void complete(){
-            writeStateMeta( jobId, Status.COMPLETE, count, System.currentTimeMillis() );
-        }
+    @Override
+    public ReIndexStatus getStatusForCollection( final String appIdString, final String collectionName ) {
+        Preconditions.checkNotNull( collectionName, "appIdString must not be null" );
+        Preconditions.checkNotNull( collectionName, "collectionName must not be null" );
+        return getIndexResponseForCollection( appIdString, collectionName );
     }
+
 
 
     /**
@@ -346,7 +350,7 @@ public class ReIndexServiceImpl implements ReIndexService {
         final String stringStatus = mapManager.getString( jobId+MAP_STATUS_KEY );
 
         if(stringStatus == null){
-           return new ReIndexStatus( jobId, Status.UNKNOWN, 0, 0 );
+           return new ReIndexStatus( jobId, Status.UNKNOWN, 0, 0, "" );
         }
 
         final Status status = Status.valueOf( stringStatus );
@@ -354,7 +358,39 @@ public class ReIndexServiceImpl implements ReIndexService {
         final long processedCount = mapManager.getLong( jobId + MAP_COUNT_KEY );
         final long lastUpdated = mapManager.getLong( jobId + MAP_UPDATED_KEY );
 
-        return new ReIndexStatus( jobId, status, processedCount, lastUpdated );
+        return new ReIndexStatus( jobId, status, processedCount, lastUpdated, "" );
+    }
+
+
+    private void writeStateMetaForCollection(final String appIdString, final String collectionName,
+                                             final Status status, final long processedCount, final long lastUpdated ) {
+
+        if(logger.isDebugEnabled()) {
+            logger.debug( "Flushing state for collection {}, status {}, processedCount {}, lastUpdated {}",
+                collectionName, status, processedCount, lastUpdated);
+        }
+
+        mapManager.putString( appIdString + MAP_SEPARATOR + collectionName + MAP_STATUS_KEY, status.name() );
+        mapManager.putLong( appIdString + MAP_SEPARATOR + collectionName + MAP_COUNT_KEY, processedCount );
+        mapManager.putLong( appIdString + MAP_SEPARATOR + collectionName + MAP_UPDATED_KEY, lastUpdated );
+    }
+
+
+    private ReIndexStatus getIndexResponseForCollection( final String appIdString, final String collectionName ) {
+
+        final String stringStatus =
+            mapManager.getString( appIdString + MAP_SEPARATOR + collectionName + MAP_STATUS_KEY );
+
+        if(stringStatus == null){
+            return new ReIndexStatus( "", Status.UNKNOWN, 0, 0, collectionName );
+        }
+
+        final Status status = Status.valueOf( stringStatus );
+
+        final long processedCount = mapManager.getLong( appIdString + MAP_SEPARATOR + collectionName + MAP_COUNT_KEY );
+        final long lastUpdated = mapManager.getLong( appIdString + MAP_SEPARATOR + collectionName + MAP_UPDATED_KEY );
+
+        return new ReIndexStatus( "", status, processedCount, lastUpdated, collectionName );
     }
 }
 

stack/rest/src/main/java/org/apache/usergrid/rest/applications/CollectionResource.java
@@ -287,24 +287,39 @@ public class CollectionResource extends ServiceResource {
     }
 
 
-    // TODO: this can't be controlled and until it can be controlled we shouldn' allow muggles to do this.
-    // So system access only.
-    // TODO: use scheduler here to get around people sending a reindex call 30 times.
+
     @POST
     @Path("{itemName}/_reindex")
     @Produces({ MediaType.APPLICATION_JSON,"application/javascript"})
-    @RequireSystemAccess
+    @RequireApplicationAccess
     @JSONP
     public ApiResponse executePostForReindexing(
-        @Context UriInfo ui, String body,
+        @Context UriInfo ui, final Map<String, Object> payload,
         @PathParam("itemName") PathSegment itemName,
         @QueryParam("callback") @DefaultValue("callback") String callback ) throws Exception {
 
         addItemToServiceContext( ui, itemName );
 
         IndexResource indexResource = new IndexResource(injector);
-        return indexResource.rebuildIndexesPost(
+        return indexResource.rebuildIndexCollectionPost(payload,
             services.getApplicationId().toString(),itemName.getPath(),false,callback );
+    }
+
+    @GET
+    @Path("{itemName}/_reindex")
+    @Produces({ MediaType.APPLICATION_JSON,"application/javascript"})
+    @RequireApplicationAccess
+    @JSONP
+    public ApiResponse executeGetForReindexStatus(
+        @Context UriInfo ui, final Map<String, Object> payload,
+        @PathParam("itemName") PathSegment itemName,
+        @QueryParam("callback") @DefaultValue("callback") String callback ) throws Exception {
+
+        addItemToServiceContext( ui, itemName );
+
+        IndexResource indexResource = new IndexResource(injector);
+        return indexResource.rebuildIndexCollectionGet(services.getApplicationId().toString(), itemName.getPath(),
+            callback );
     }
 
 
@@ -346,18 +361,17 @@ public class CollectionResource extends ServiceResource {
     private ApiResponse executeAndCreateResponse(final CollectionDeleteRequestBuilder request, final String callback ) {
 
 
-        final CollectionDeleteService.CollectionDeleteStatus status = getCollectionDeleteService().deleteCollection( request );
+        final CollectionDeleteService.CollectionDeleteStatus status = getCollectionDeleteService().deleteCollection(request);
 
         final ApiResponse response = createApiResponse();
 
-        response.setAction( "clear collection" );
-        response.setProperty( "jobId", status.getJobId() );
-        response.setProperty( "status", status.getStatus() );
-        response.setProperty( "lastUpdatedEpoch", status.getLastUpdated() );
-        response.setProperty( "numberQueued", status.getNumberProcessed() );
+        response.setAction("clear collection");
+        response.setProperty("jobId", status.getJobId());
+        response.setProperty("status", status.getStatus());
+        response.setProperty("lastUpdatedEpoch", status.getLastUpdated());
+        response.setProperty("numberQueued", status.getNumberProcessed());
         response.setSuccess();
 
         return response;
     }
-
 }

commit a619b1f41b0f0d552cb8fc28179bb6e7bd76bc1c

Fixed failing tests and bugs -Fixed tests for Core module that were broken due to changes in aa44b06 -Fixed failing tests for Services module related to threading issues -Fixed failing tests for Rest module related to Index rebuild -Fixed re-index code to ensure rebuild status is correctly maintained -Updated Shiro to 1.3.2 (latest)

stack/core/src/main/java/org/apache/usergrid/corepersistence/index/ReIndexService.java
@@ -97,7 +97,7 @@ public interface ReIndexService {
         }
 
         /**
-         * Get the jobId used to resume this operation
+         * Get the collectionName used to resume this operation
          */
         public String getCollectionName() {
             return collectionName;

stack/core/src/main/java/org/apache/usergrid/corepersistence/index/ReIndexServiceImpl.java
@@ -202,7 +202,7 @@ public class ReIndexServiceImpl implements ReIndexService {
             .subscribeOn( Schedulers.io() ).subscribe();
 
         if(isForCollection){
-            return new ReIndexStatus( "", Status.STARTED, 0, 0, reIndexRequestBuilder.getCollectionName().get() );
+            return new ReIndexStatus( "", Status.STARTED, 0, 0, CpNamingUtils.getNameFromEdgeType(reIndexRequestBuilder.getCollectionName().get()) );
 
         }
 
@@ -365,21 +365,23 @@ public class ReIndexServiceImpl implements ReIndexService {
     private void writeStateMetaForCollection(final String appIdString, final String collectionName,
                                              final Status status, final long processedCount, final long lastUpdated ) {
 
-        if(logger.isDebugEnabled()) {
+    	String prefixedColName = CpNamingUtils.getEdgeTypeFromCollectionName( collectionName.toLowerCase() );
+    	if(logger.isDebugEnabled()) {
             logger.debug( "Flushing state for collection {}, status {}, processedCount {}, lastUpdated {}",
-                collectionName, status, processedCount, lastUpdated);
+            		collectionName, status, processedCount, lastUpdated);
         }
 
-        mapManager.putString( appIdString + MAP_SEPARATOR + collectionName + MAP_STATUS_KEY, status.name() );
-        mapManager.putLong( appIdString + MAP_SEPARATOR + collectionName + MAP_COUNT_KEY, processedCount );
-        mapManager.putLong( appIdString + MAP_SEPARATOR + collectionName + MAP_UPDATED_KEY, lastUpdated );
+        mapManager.putString( appIdString + MAP_SEPARATOR + prefixedColName + MAP_STATUS_KEY, status.name() );
+        mapManager.putLong( appIdString + MAP_SEPARATOR + prefixedColName + MAP_COUNT_KEY, processedCount );
+        mapManager.putLong( appIdString + MAP_SEPARATOR + prefixedColName + MAP_UPDATED_KEY, lastUpdated );
     }
 
 
     private ReIndexStatus getIndexResponseForCollection( final String appIdString, final String collectionName ) {
 
-        final String stringStatus =
-            mapManager.getString( appIdString + MAP_SEPARATOR + collectionName + MAP_STATUS_KEY );
+        String prefixedColName = CpNamingUtils.getEdgeTypeFromCollectionName( collectionName.toLowerCase() );
+    	final String stringStatus =
+            mapManager.getString( appIdString + MAP_SEPARATOR + prefixedColName + MAP_STATUS_KEY );
 
         if(stringStatus == null){
             return new ReIndexStatus( "", Status.UNKNOWN, 0, 0, collectionName );
@@ -387,8 +389,8 @@ public class ReIndexServiceImpl implements ReIndexService {
 
         final Status status = Status.valueOf( stringStatus );
 
-        final long processedCount = mapManager.getLong( appIdString + MAP_SEPARATOR + collectionName + MAP_COUNT_KEY );
-        final long lastUpdated = mapManager.getLong( appIdString + MAP_SEPARATOR + collectionName + MAP_UPDATED_KEY );
+        final long processedCount = mapManager.getLong( appIdString + MAP_SEPARATOR + prefixedColName + MAP_COUNT_KEY );
+        final long lastUpdated = mapManager.getLong( appIdString + MAP_SEPARATOR + prefixedColName + MAP_UPDATED_KEY );
 
         return new ReIndexStatus( "", status, processedCount, lastUpdated, collectionName );
     }

stack/pom.xml
@@ -114,7 +114,7 @@
         <junit-version>4.12</junit-version>
         <log4j-version>1.2.16</log4j-version>
         <org.springframework.version>3.2.13.RELEASE</org.springframework.version>
-        <shiro-version>1.2.4</shiro-version>
+        <shiro-version>1.3.2</shiro-version>
         <slf4j-version>1.7.2</slf4j-version>
         <snakeyaml-version>1.9</snakeyaml-version>
         <tomcat-version>7.0.64</tomcat-version>

stack/core/src/test/java/org/apache/usergrid/persistence/RebuildIndexTest.java
@@ -154,12 +154,12 @@ public class RebuildIndexTest extends AbstractCoreIT {
 
         ReIndexService.ReIndexStatus status = reIndexService.rebuildIndex(builder);
 
-        assertNotNull(status.getJobId(), "JobId is present");
+        assertNotNull(status.getCollectionName(), "Collection name is present");
 
         logger.info("Rebuilt index");
 
 
-        waitForRebuild(status, reIndexService);
+        waitForRebuild(em.getApplicationId().toString(), status.getCollectionName(), reIndexService);
 
         //app.waitForQueueDrainAndRefreshIndex(15000);
 
@@ -279,7 +279,7 @@ public class RebuildIndexTest extends AbstractCoreIT {
             logger.info("Rebuilt index, jobID={}", status.getJobId());
 
 
-            waitForRebuild(status, reIndexService);
+            waitForRebuild(status.getJobId(), reIndexService);
 
 
             logger.info("Rebuilt index");
@@ -387,7 +387,7 @@ public class RebuildIndexTest extends AbstractCoreIT {
 
             logger.info("Rebuilt index");
 
-            waitForRebuild(status, reIndexService);
+            waitForRebuild(status.getJobId(), reIndexService);
 
             logger.info("Rebuilt index");
 
@@ -486,7 +486,7 @@ public class RebuildIndexTest extends AbstractCoreIT {
 
             logger.info("Rebuilt index");
 
-            waitForRebuild(status, reIndexService);
+            waitForRebuild(status.getJobId(), reIndexService);
 
             logger.info("Rebuilt index");
 
@@ -505,18 +505,53 @@ public class RebuildIndexTest extends AbstractCoreIT {
     /**
      * Wait for the rebuild to occur
      */
-    private void waitForRebuild(final ReIndexService.ReIndexStatus status, final ReIndexService reIndexService)
+    private void waitForRebuild(final String jobId, final ReIndexService reIndexService)
         throws InterruptedException, IllegalArgumentException {
-        if (status != null) {
-            logger.info("waitForRebuild: jobID={}", status.getJobId());
+        if (jobId != null && !jobId.trim().equals("")) {
+            logger.info("waitForRebuild: jobID={}", jobId);
         } else {
-            logger.info("waitForRebuild: error, status = null");
-            throw new IllegalArgumentException("reindexStatus = null");
+            logger.info("waitForRebuild: error, jobId = null or empty");
+            throw new IllegalArgumentException("jobId = null or empty");
         }
         while (true) {
 
             try {
-                final ReIndexService.ReIndexStatus updatedStatus = reIndexService.getStatus(status.getJobId());
+                final ReIndexService.ReIndexStatus updatedStatus = reIndexService.getStatus(jobId);
+
+                if (updatedStatus == null) {
+                    logger.info("waitForRebuild: updated status is null");
+                } else {
+                    logger.info("waitForRebuild: status={} numberProcessed={}", updatedStatus.getStatus().toString(), updatedStatus.getNumberProcessed());
+
+                    if (updatedStatus.getStatus() == ReIndexService.Status.COMPLETE) {
+                        break;
+                    }
+                }
+            } catch (IllegalArgumentException iae) {
+                //swallow.  Thrown if our job can't be found.  I.E hasn't updated yet
+            }
+
+
+            Thread.sleep(1000);
+        }
+    }
+    
+    
+    /**
+     * Wait for the rebuild to occur
+     */
+    private void waitForRebuild(final String appId, final String collectionName, final ReIndexService reIndexService)
+        throws InterruptedException, IllegalArgumentException {
+        if (appId != null && !appId.trim().equals("") && collectionName != null && !collectionName.trim().equals("")) {
+            logger.info("waitForRebuild: appId={} collName={}", appId, collectionName);
+        } else {
+            logger.info("waitForRebuild: error, appId or collName = null or empty");
+            throw new IllegalArgumentException("appId or collName = null or empty");
+        }
+        while (true) {
+
+            try {
+                final ReIndexService.ReIndexStatus updatedStatus = reIndexService.getStatusForCollection(appId, collectionName);
 
                 if (updatedStatus == null) {
                     logger.info("waitForRebuild: updated status is null");

stack/rest/src/test/java/org/apache/usergrid/rest/IndexResourceIT.java
@@ -25,6 +25,7 @@ import org.apache.usergrid.rest.test.resource.AbstractRestIT;
 import org.apache.usergrid.rest.test.resource.model.ApiResponse;
 import org.apache.usergrid.rest.test.resource.model.QueryParameters;
 import org.apache.usergrid.rest.test.resource.model.Token;
+import org.apache.usergrid.corepersistence.index.ReIndexService.Status;
 import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -68,35 +69,51 @@ public class IndexResourceIT extends AbstractRestIT {
             .get(clientSetup.getSuperuserName(),clientSetup.getSuperuserPassword());
 
         QueryParameters queryParameters = new QueryParameters();
-        queryParameters.addParam( "access_token",superUserToken.getAccessToken());
+        queryParameters.addParam("access_token", superUserToken.getAccessToken());
         ApiResponse result = clientSetup.getRestClient()
-            .pathResource( "system/index/rebuild/" + clientSetup.getAppUuid() + "/StOrElaTloNs" )
-            .post( false, ApiResponse.class, null, queryParameters, true );
+            .pathResource("system/index/rebuild/" + clientSetup.getAppUuid() + "/StOrElaTloNs")
+            .post(false, ApiResponse.class, null, queryParameters, true);
 
         assertNotNull(result);
+        assertEquals(Status.STARTED.name(), result.getStatus());
 
         //try the reindex endpoint with all lowercase Characters
         queryParameters = new QueryParameters();
-        queryParameters.addParam( "access_token",clientSetup.getSuperuserToken().getAccessToken() );
+        queryParameters.addParam("access_token", superUserToken.getAccessToken());
         result = clientSetup.getRestClient()
-            .pathResource( "system/index/rebuild/"+clientSetup.getAppUuid()+"/storelatlons" )
-            .post( false, ApiResponse.class,null,queryParameters,true);
-        String status = result.getProperties().get("jobId").toString();
-
-        assertNotNull( result );
+            .pathResource("system/index/rebuild/" + clientSetup.getAppUuid() + "/storelatlons")
+            .post(false, ApiResponse.class, null, queryParameters, true);
+        
+        assertNotNull(result);
+        //at this point, this could return a result of the previous reindex, or if it has completed, it will create a new job
+        assertNotEquals(Status.UNKNOWN.name(), result.getStatus());
 
         WebTarget res = clientSetup.getRestClient()
-            .pathResource( "system/index/rebuild/" + result.getProperties()
-                .get( "jobId" ).toString() )
+            .pathResource("system/index/rebuild/" + clientSetup.getAppUuid() + "/storelatlons")
             .getTarget();
 
         HttpAuthenticationFeature feature = HttpAuthenticationFeature.basicBuilder()
             .credentials( "superuser", "superpassword" ).build();
 
         result = res.register( feature ).request().get( ApiResponse.class );
-
-        assertNotNull( result );
-        assertEquals(status,result.getProperties().get("jobId").toString());
+        assertNotNull(result);
+        
+        int retry = 0;
+        while(retry < 5 && !result.getStatus().equals(Status.COMPLETE.name())) {
+        	try {
+        		//hope reindex completes, if not, that's still ok
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				
+			}
+        	result = res.register( feature ).request().get( ApiResponse.class );
+        	retry++;
+        	assertNotNull(result);
+        }
+        
+        Map<String, Object> resultMap = result.getProperties();
+        assertNotNull( resultMap );
+        assertEquals(1,resultMap.get("numberQueued"));
 
 
     }

stack/services/src/test/java/org/apache/usergrid/NewOrgAppAdminRule.java
@@ -120,6 +120,7 @@ public class NewOrgAppAdminRule implements TestRule {
      * Create the org admin and application
      */
     protected void before( Description description ) throws Exception {
+    	logger.info( "Test {}: Starting with application", description.getDisplayName() );
         final String className = description.getClassName();
         final String methodName = description.getMethodName();
         final String uuidString = newUUIDString();

stack/services/src/test/java/org/apache/usergrid/management/OrganizationConfigIT.java
@@ -32,12 +32,14 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 
+import net.jcip.annotations.NotThreadSafe;
+
 import java.util.*;
 
 import static org.apache.usergrid.TestHelper.*;
 import static org.junit.Assert.*;
 
-
+@NotThreadSafe
 public class OrganizationConfigIT {
 
     @Rule

stack/services/src/test/java/org/apache/usergrid/management/export/ExportServiceIT.java
@@ -59,6 +59,8 @@ import com.amazonaws.SDKGlobalConfiguration;
 import com.google.common.collect.ImmutableSet;
 import com.google.inject.Module;
 
+import net.jcip.annotations.NotThreadSafe;
+
 import static org.apache.usergrid.TestHelper.newUUIDString;
 import static org.apache.usergrid.TestHelper.uniqueApp;
 import static org.apache.usergrid.TestHelper.uniqueOrg;
@@ -74,6 +76,7 @@ import static org.mockito.Mockito.when;
  *
  *
  */
+@NotThreadSafe
 public class ExportServiceIT {
 
     private static final Logger logger = LoggerFactory.getLogger( ExportServiceIT.class );